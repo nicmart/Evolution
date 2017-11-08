5ba0d9952ced281ad6e9799e5a8a74c84101039a
Nicolò Martini <nicolo@martini.io>
2017-11-08 21:41:13 +0000

    dev and opt version for index.html

--- server/src/main/scala/Server.scala
+++ server/src/main/scala/Server.scala
@@ -14,11 +14,11 @@ object WebServer {
     implicit val executionContext = system.dispatcher
 
     val route =
-      pathPrefix("assets" / Remaining) { file =>
+      pathPrefix(Remaining) { file =>
         // optionally compresses the response with Gzip or Deflate
         // if the client accepts compressed responses
         encodeResponse {
-          getFromDirectory("public/" + file)
+          getFromResource("public/" + file)
         }
       }
 
