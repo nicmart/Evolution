3f468576ba2de96fedd5430e4d4c41fe0d5793ec
Nicolò Martini <nicolo@martini.io>
2017-12-15 22:51:46 +0000

    Completed parser for existing drawing algebra

--- core/src/main/scala/evolution/drawing/algebra/parser/DrawingParser.scala
+++ core/src/main/scala/evolution/drawing/algebra/parser/DrawingParser.scala
@@ -2,6 +2,7 @@ package evolution.drawing.algebra.parser
 
 import evolution.drawing.algebra.interpreter.Builder
 import evolution.drawing.algebra.{Drawing, DrawingAlgebra}
+import evolution.geometry.Point
 import fastparse.{all, core}
 import fastparse.all._
 
@@ -13,27 +14,62 @@ trait DrawingParser[+A] {
 object DrawingParser {
   type Error = String
 
-  object DoubleDrawingParser extends DrawingParser[Double] {
+  object Parsers {
     val digit: Parser[Unit] =
       P(CharIn('0' to '9'))
     val floatDigits: all.Parser[Unit] =
       P(digit.rep ~ "." ~ digit.rep(1))
     val double: Parser[Double] =
       P((floatDigits | digit.rep(1)).!.map(_.toDouble))
+    val point: Parser[Point] =
+      P("point(" ~ double ~ "," ~ double ~ ")").map { case (x, y) => Point(x, y) }
+    val constPoint: Parser[Drawing[Point]] =
+      P("point(" ~ double ~ "," ~ double ~ ")").map { case (x, y) => Builder.cartesian(Builder.const(x), Builder.const(y)) }
 
     val const: Parser[Drawing[Double]] =
       P(double | "const(" ~ double ~ ")").map(Builder.const)
+
+    val cartesian: Parser[Drawing[Point]] =
+      P("cartesian(" ~ doubleDrawing ~ "," ~ doubleDrawing ~ ")").map { case (x, y) => Builder.cartesian(x, y)}
+
+    val polar: Parser[Drawing[Point]] =
+      P("polar(" ~ doubleDrawing ~ "," ~ doubleDrawing ~ ")").map { case (x, y) => Builder.polar(x, y)}
+
     val rnd: Parser[Drawing[Double]] =
       P("rnd(" ~ double ~ "," ~ double ~ ")").map { case (x, y) => Builder.rnd(x, y) }
+
     val integrateDouble: Parser[Drawing[Double]] =
       P("integrateDouble(" ~ double ~ "," ~ doubleDrawing ~ ")").map { case (start, f) => Builder.integrateDouble(start, f) }
-    lazy val doubleDrawing: Parser[Drawing[Double]] =
-      P(const | rnd | integrateDouble)
 
-    override def parse(s: String): Either[String, Drawing[Double]] =
-      toEither(doubleDrawing.parse(s))
+    val integratePoint: Parser[Drawing[Point]] =
+      P("integratePoint(" ~ point ~ "," ~ pointDrawing ~ ")").map { case (start, f) => Builder.integratePoint(start, f) }
+
+    val deriveDouble: Parser[Drawing[Double]] =
+      P("deriveDouble(" ~ doubleDrawing ~ ")").map { Builder.deriveDouble }
+
+    val derivePoint: Parser[Drawing[Point]] =
+      P("derivePoint(" ~ pointDrawing ~ ")").map { Builder.derivePoint }
+
+    lazy val doubleDrawing: Parser[Drawing[Double]] =
+      P(const | rnd | integrateDouble | deriveDouble)
+
+    lazy val pointDrawing: Parser[Drawing[Point]] =
+      P(cartesian | polar | constPoint | integratePoint | derivePoint)
   }
 
-  def toEither[T, Elem, Repr](result: core.Parsed[T, Elem, Repr]): Either[String, T] =
+  implicit object DoubleDrawingParser extends DrawingParser[Double] {
+    override def parse(s: String): Either[String, Drawing[Double]] =
+      toEither(Parsers.doubleDrawing.parse(s))
+  }
+
+  implicit object PointDrawingParser extends DrawingParser[Point] {
+    override def parse(s: String): Either[String, Drawing[Point]] =
+      toEither(Parsers.pointDrawing.parse(s))
+  }
+
+  def parse[T](s: String)(implicit parser: DrawingParser[T]): Either[String, Drawing[T]] =
+    parser.parse(s)
+
+  private def toEither[T, Elem, Repr](result: core.Parsed[T, Elem, Repr]): Either[String, T] =
     result.fold((_, _, _) => Left("error"), (t, _) => Right(t))
 }
--- core/src/test/scala/evolution/algebra/drawing/parser/ParserSpec.scala
+++ core/src/test/scala/evolution/algebra/drawing/parser/ParserSpec.scala
@@ -6,6 +6,9 @@ import evolution.drawing.algebra.parser.DrawingParser.DoubleDrawingParser
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.prop.PropertyChecks
 import evolution.drawing.algebra.interpreter.Builder._
+import evolution.drawing.algebra.parser.DrawingParser
+import evolution.geometry.Point
+import fastparse.all
 
 class ParserSpec
   extends WordSpec
@@ -14,26 +17,49 @@ class ParserSpec
 
   "A Parser" should {
     "parse a rnd expression" in {
-      assertSuccessfulParse("rnd(0.1,1)", rnd(0.1, 1))
-      assertSuccessfulParse("rnd(.1,1)", rnd(0.1, 1))
-      assertSuccessfulParse("rnd(.1,1.001)", rnd(0.1, 1.001))
+      assertParse("rnd(0.1,1)", rnd(0.1, 1))
+      assertParse("rnd(.1,1)", rnd(0.1, 1))
+      assertParse("rnd(.1,1.001)", rnd(0.1, 1.001))
     }
     "parse a const expression" in {
-      assertSuccessfulParse("const(0.1)", const(0.1))
-      assertSuccessfulParse("const(.1)", const(0.1))
-      assertSuccessfulParse(".123", const(0.123))
+      assertParse("const(0.1)", const(0.1))
+      assertParse("const(.1)", const(0.1))
+      assertParse(".123", const(0.123))
     }
     "parse an integrateDouble expression" in {
-      assertSuccessfulParse("integrateDouble(.1,.1)", integrateDouble(.1, const(0.1)))
-      assertSuccessfulParse(
+      assertParse("integrateDouble(.1,.1)", integrateDouble(.1, const(0.1)))
+      assertParse(
         "integrateDouble(.1,integrateDouble(0,const(.1)))",
         integrateDouble(.1, integrateDouble(0, const(0.1)))
       )
     }
+    "parse a deriveDouble expression" in {
+      assertParse("deriveDouble(1)", deriveDouble(const(1)))
+    }
+    "parse an integratePoint expression" in {
+      assertParse("integratePoint(point(0,0),point(1,1))", integratePoint(Point.zero, cartesian(const(1), const(1))))
+    }
+    "parse a derivePoint expression" in {
+      assertParse("derivePoint(point(1,1))", derivePoint(cartesian(const(1), const(1))))
+    }
+    "parse an cartesian expression" in {
+      assertParse("cartesian(.1,.1)", cartesian(const(.1), const(0.1)))
+      assertParse(
+        "cartesian(.1,integrateDouble(0,const(.1)))",
+        cartesian(const(.1), integrateDouble(0, const(0.1)))
+      )
+    }
+    "parse a polar expression" in {
+      assertParse("polar(.1,.1)", polar(const(.1), const(0.1)))
+      assertParse(
+        "polar(.1,integrateDouble(0,const(.1)))",
+        polar(const(.1), integrateDouble(0, const(0.1)))
+      )
+    }
   }
 
-  def assertSuccessfulParse(serializedDrawing: String, expected: Drawing[Double]) = {
-    val actual = DoubleDrawingParser.parse(serializedDrawing)
+  def assertParse[T](serializedDrawing: String, expected: Drawing[T])(implicit parser: DrawingParser[T]) = {
+    val actual = parser.parse(serializedDrawing)
     actual.map(_.run(Serializer)) shouldBe Right(expected.run(Serializer))
   }
 }
