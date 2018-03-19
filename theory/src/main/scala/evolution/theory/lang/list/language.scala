21fd4bd276e1dd50200f36894bf395f24b38a771
Nicolò Martini <nicolo@martini.io>
2018-03-19 20:39:29 +0000

    Crazy tries for an encoding of tower of languages with bindings

--- theory/src/main/scala/evolution/theory/lang/list/language.scala
+++ theory/src/main/scala/evolution/theory/lang/list/language.scala
@@ -1,66 +1,104 @@
 package evolution.theory.lang.list
 
+import evolution.theory.lang.list.Expr.{ExprF, Fn}
+
 trait BindingLang {
   type ~>[A, B]
-  def var0[E, A]: (A, E) ~> A
-  def varS[E, A, B](expr: E ~> A): (B, E) ~> A
-  def fix[E, A](f: (A, E) ~> A): E ~> A
+  def var0[A]: A ~> A
+  def varS[A, B](expr: A): B ~> A
+  def fix[A](f: A ~> A): A
 }
 
-trait Lang extends BindingLang {
+trait Lang extends BindingLang { self =>
   type F[A]
-  def empty[E, A]: E ~> F[A]
-  def cons[E, A](a: A, tail: E ~> F[A]): E ~> F[A]
-  def mapEmpty[E, A](fa1: E ~> F[A], fa2: E ~> F[A]): E ~> F[A]
-  def mapCons[E, A, B](fa1: E ~> F[A])(f: (A, (F[A], E)) ~> F[B]): E ~> F[B]
+  type C[A]
+  type ~>[A, B]
+  def next[X]: Lang.Aux[λ[T => X ~> F[T]], λ[T => X ~> C[T]], ~>]
+  final type Apply[T[F[_], C[_], ~>[_, _]]] = T[F, C, ~>]
+  def value[A](a: A): C[A]
+  def empty[A]: F[A]
+  def cons[A](a: C[A], tail: F[A]): F[A]
+  def mapEmpty[A](fa1: F[A], fa2: F[A]): F[A]
+  def mapCons[A, B](fa1: F[A])(f: C[A] ~> F[A] ~> F[B]): F[B]
 }
 
-trait Expr[E[_[_]], A] {
-  def run(alg: Lang): alg.~>[E[alg.F], alg.F[A]]
+object Lang {
+  type Aux[FF[_], CC[_], ~~>[_, _]] = Lang {
+    type F[A] = FF[A]
+    type C[A] = CC[A]
+    type ~>[A, B] = ~~>[A, B]
+  }
 }
 
-class Expressions[E[_[_]]] {
-  type Next[A, F[_]] = (F[A], E[F])
-  type NextC[A, F[_]] = (A, E[F])
-  type NextCons[A, F[_]] = (A, (F[A], E[F]))
-  type ExprS[A, B] = Expr[Next[A, ?[_]], B]
-  type ExprSC[A, B] = Expr[NextC[A, ?[_]], B]
-  type ExprCons[A, B] = Expr[NextCons[A, ?[_]], B]
-  def next[A]: Expressions[Next[A, ?[_]]] =
-    new Expressions[Next[A, ?[_]]]
+trait Expr[A[F[_], C[_], ~>[_, _]]] { self =>
+  def run(alg: Lang): alg.Apply[A]
+  def next[E[F[_], C[_], ~>[_, _]]]: Expr[Fn[E, A]#T] = new Expr[Fn[E, A]#T] {
+    override def run(alg: Lang): alg.~>[alg.Apply[E], alg.Apply[A]] =
+      ???
+      //self.run(alg.next[alg.Apply[E]])
+  }
+}
 
-  def cons[A](a: A, tail: Expr[E, A]): Expr[E, A] =
-    new Expr[E, A] {
-      override def run(alg: Lang): alg.~>[E[alg.F], alg.F[A]] =
-        alg.cons(a, tail.run(alg))
+object Expr {
+  trait T[A] {
+    final type FT[F[_], C[_], ~>[_, _]] = F[A]
+    final type CT[F[_], C[_], ~>[_, _]] = C[A]
+    final type Self[F[_], C[_], ~>[_, _]] = A
+  }
+  trait Fn[From[F[_], C[_], ~>[_, _]], To[F[_], C[_], ~>[_, _]]] {
+    final type T[F[_], C[_], ~>[_, _]] = From[F, C, ~>] ~> To[F, C, ~>]
+  }
+
+  trait ExprPlain[A] extends Expr[T[A]#Self]
+  trait ExprF[A] extends Expr[T[A]#FT]
+  trait ExprC[A] extends Expr[T[A]#CT]
+  trait ExprFnF[A, B] extends Expr[Fn[T[A]#FT, T[B]#FT]#T]
+  trait ExprFn[A, B] extends Expr[Fn[T[A]#Self, T[B]#Self]#T]
+}
+
+trait ValueExpr[A] {
+  def run(alg: Lang): alg.C[A]
+}
+
+class Expressions {
+  import Expr._
+
+  def value[A](a: A): ExprC[A] = new ExprC[A] {
+    override def run(alg: Lang): alg.C[A] = alg.value(a)
+  }
+
+  def cons[A](a: ExprC[A], tail: ExprF[A]): ExprF[A] =
+    new ExprF[A] {
+      override def run(alg: Lang): alg.F[A] =
+        alg.cons(a.run(alg), tail.run(alg))
     }
 
-  def var0[A]: Expr[Next[A, ?[_]], A] =
-    new Expr[Next[A, ?[_]], A] {
-      override def run(alg: Lang): alg.~>[(alg.F[A], E[alg.F]), alg.F[A]] =
+  def var0[A]: ExprFn[A, A] =
+    new ExprFn[A, A] {
+      override def run(alg: Lang): alg.Apply[Fn[T[A]#Self, T[A]#Self]#T] =
         alg.var0
     }
 
-  def fix[A](expr: Expr[Next[A, ?[_]], A]): Expr[E, A] =
-    new Expr[E, A] {
-      override def run(alg: Lang): alg.~>[E[alg.F], alg.F[A]] =
+  def fix[A](expr: ExprFn[A, A]): ExprPlain[A] =
+    new ExprPlain[A] {
+      override def run(alg: Lang): A =
         alg.fix(expr.run(alg))
     }
 
-  def mapEmpty[A](fa1: Expr[E, A], fa2: Expr[E, A]): Expr[E, A] =
-    new Expr[E, A] {
-      override def run(alg: Lang): alg.~>[E[alg.F], alg.F[A]] =
+  def mapEmpty[A](fa1: ExprF[A], fa2: ExprF[A]): ExprF[A] =
+    new ExprF[A] {
+      override def run(alg: Lang): alg.F[A] =
         alg.mapEmpty(fa1.run(alg), fa2.run(alg))
     }
-
-  def mapCons[A, B](fa1: Expr[E, A])(f: ExprCons[A, B]): Expr[E, B] =
-    new Expr[E, B] {
-      override def run(alg: Lang): alg.~>[E[alg.F], alg.F[B]] =
-        alg.mapCons(fa1.run(alg))(f.run(alg))
-    }
-
-  def constant[A](a: A): Expr[E, A] =
-    fix(next[A].cons(a, var0))
+//
+//  def mapCons[A, B](fa1: Expr[E, A])(f: ExprCons[A, B]): Expr[E, B] =
+//    new Expr[E, B] {
+//      override def run(alg: Lang): alg.~>[E[alg.F], alg.F[B]] =
+//        alg.mapCons(fa1.run(alg))(f.run(alg))
+//    }
+//
+//  def constant[A](a: A): ExprF[A] =
+//    fix(next[A].cons(a, var0))
 }
 
 trait Serializer extends Lang {
