24ab14782e5e7bfcf208b0b048d627e3c6dbf276
Nicolò Martini <nicolo@martini.io>
2018-04-04 14:06:03 +0100

    Finally define integrate in the new experimental algebra

--- theory/src/main/scala/evolution/theory/lang/list/language.scala
+++ theory/src/main/scala/evolution/theory/lang/list/language.scala
@@ -7,8 +7,8 @@ trait BindingLang {
   type Env[A]
   def var0[A]: A ~> Env[A]
   def varS[A, B](expr: Env[A]): B ~> Env[A]
-  def ap[A, B](f: A ~> B)(a: A): B
-  def ap2[A, B, C](f: A ~> (B ~> C))(a: A, b: B): C = ap(ap(f)(a))(b)
+  def ap[A, B](f: Env[A ~> B])(a: Env[A]): Env[B]
+  def ap2[A, B, C](f: Env[A ~> (B ~> C)])(a: Env[A], b: Env[B]): Env[C] = ap(ap(f)(a))(b)
   def fix[A](f: A ~> Env[A]): A
 }
 
@@ -24,8 +24,12 @@ trait Lang extends BindingLang { self =>
   def mapEmpty[A](fa1: F[A], fa2: F[A]): F[A]
   def mapCons[A, B](fa1: F[A])(f: C0[A] ~> (F0[A] ~> F[B])): F[B]
 
+  // Fine here, we are still in the applicative domain
   def mapC[A, B](ca: C[A])(f: A => B): C[B]
   def mapF[A, B](ca: F[A])(f: A => B): F[B]
+  // Not sure if this is more a zipWith rather than a map2
+  def map2F[A, B, Z](fa: F[A], fb: F[B])(f: (A, B) => Z): F[Z]
+  def map2C[A, B, Z](fa: C[A], fb: C[B])(f: (A, B) => Z): C[Z]
 
   trait LangNext[X] extends Lang {
     final type Env[A] = X ~> self.Env[A]
@@ -59,51 +63,39 @@ class Expressions(val lang: Lang.Base) {
 
 
   def integrate: C[Double] ~> (F[Double] ~> F[Double]) = {
-    // b1 works on F[Double] ~> *
+    // velocity evolution
     val b1 = next[F[Double]]
-    // This compiles
-    val y1: F[Double] ~> Env[F[Double]] = b1.x
-    val y11: b1.Env[F[Double]] = b1.x
-    // b2 works on C[Double] ~> (F[Double] ~> *)
+    // from constant
     val b2 = b1.next[C[Double]]
-    // This compiles
-    val y2: C[Double] ~> b1.Env[C[Double]] = b2.x
-    val y21: C[Double] ~> (F[Double] ~> Env[C[Double]]) = b2.x
-    // b3 works on (C[Double] ~> (F[Double] ~> F[Double])) ~> (C[Double] ~> (F[Double] ~> *))
-    // In b3, b2.var0[C[Double] ~> (F[Double] ~> F[Double])] is integrate
-    // b3.x
-    // b2.varS(b1.var0[C[Double]]) is the "from" b2.varS(b2.x)
-    // b2.varS(b1.varS(var0[F[Double]])) is the speed (b2.varS(b1.varS(b1.x)))
+    // integrate recursive call
     val b3 = b2.next[C[Double] ~> (F[Double] ~> F[Double])]
-    //val y3: (C[Double] ~> (F[Double] ~> F[Double])) ~> (C[Double] ~> (F[Double] ~> Env[C[Double]])) = b3.x
+
     val y3: (C[Double] ~> (F[Double] ~> F[Double])) ~> (C[Double] ~> (F[Double] ~> Env[C[Double] ~> (F[Double] ~> F[Double])])) = b3.x
-    //val integrateVar = b3.x
-    //val fromVar = b2.varS[C[Double] ~> C[Double], C[Double] ~> (F[Double] ~> F[Double])](b1.var0[C[Double]])
-
-    // Inside fix we need (C[D] ~> (F[D] ~> F[D])) ~> Env[(C[D] ~> F[D] ~> F[D])]
-    // Inside fix we need (C[D] ~> (F[D] ~> F[D])) ~> (C[D] ~> (F[D] ~> F[D]))
-    // The F[] for mapCons must then be (C[D] ~> F[D] ~> F[D]) ~> C[D] ~> F[D] ~> F[_]
-    // That means we first push F[D], then C[D], and then (C[D] ~> F[D] ~> F[D])
-
-    val b11 = next[C[Double] ~> (F[Double] ~> F[Double])]
-    //val b21 = b11.next[]
-
-    b1.mapCons(b1.x)(???)
-    b2.mapCons[Double, Double](b2.up(b1.x))(???)
 
     // tail of mapCons
     val b4 = b3.next[F[Double]]
     // head of mapCons
     val b5 = b4.next[C[Double]]
 
+    type LastEnv[X] = C[Double] ~> (F[Double] ~> ((C[Double] ~> (F[Double] ~> F[Double])) ~> (C[Double] ~> (F[Double] ~> Env[X]))))
+
+    val from: LastEnv[C[Double]] =
+      b5.up(b4.up(b3.up(b2.x)))
+    val vhead: LastEnv[C[Double]] = b5.x
+    val vtail: LastEnv[F[Double]] = b5.up(b4.x)
+    val integrateRec: LastEnv[C[Double] ~> (F[Double] ~> F[Double])] = b5.up(b4.up(b3.x))
+
     fix[C[Double] ~> (F[Double] ~> F[Double])](
       // MapCons here needs a C[Double] ~> (F[Double] ~> F[Double])  as "first argument"
       // and a C[D] ~> F[D] ~> F[D] as "second argument", BUT here C and F are the ones in b3, oh no!!!
 
       b3.mapCons(b3.up(b2.up(b1.x)))(
-        b5.cons[Double](
-          b5.up(b4.up(b3.up(b2.x))),
-          b5.up(b4.x)
+        b5.cons(
+          from,
+          b5.ap2(integrateRec)(
+            b5.map2C(vhead, from)(_ + _),
+            vtail
+          )
         )
       )
       //ap2[C[Double], F[Double], F[Double]](b3.mapCons[Double, Double](???)(???))(???, ???)
