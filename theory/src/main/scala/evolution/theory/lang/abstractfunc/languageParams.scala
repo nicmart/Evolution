55ada930d8f32d53e6771c2fb419b4fd4aac792f
Nicolò Martini <nicolo@martini.io>
2018-06-08 10:25:05 +0100

    Trying some simplifications in experimental language

--- theory/src/main/scala/evolution/theory/lang/abstractfunc/languageParams.scala
+++ theory/src/main/scala/evolution/theory/lang/abstractfunc/languageParams.scala
@@ -2,42 +2,56 @@ package evolution.theory.lang.abstractfunc.params
 
 import scala.language.higherKinds
 
-trait BindingLang[~>[_, _]] {
-  def var0[A]: A ~> A
-  def varS[A, B](expr: A): B ~> A
-  def ap[A, B](f: A ~> B)(a: A): B
-  def ap2[A, B, C](f: A ~> (B ~> C))(a: A, b: B): C = ap(ap(f)(a))(b)
-  def fix[A](f: A ~> A): A
+trait BindingLang[~>[_, _], E[_]] {
+  def var0[A]: A ~> E[A]
+  def varS[A, B](expr: E[A]): B ~> E[A]
+  def ap[A, B](f: A ~> E[B])(a: A): E[B] // Shall we wrap all the function into a single E?
+  def fix[A](f: A ~> E[A]): E[A]
 }
 
-// 1) A simple unityped language for integers, without explicit environment
-trait Lang[~>[_, _], T] extends BindingLang[~>] { self =>
+trait Lang[T] { self =>
   def int(n: Int): T
   def sum(a: T, b: T): T
-  def diff(a: T, b: T): T
-  def mul(a: T, b: T): T
-  def ifLessOrEqualThan(p: T)(a: T, b: T): T
-  def next[A]: Lang[~>, A ~> T]
 }
 
-class IntExpressions[~>[_, _], E[_], T](val lang: Lang[~>, T]) {
-  import lang._
-  def factorial(n: T): T = {
-    val b1 = lang.next[T ~> T]
-    val b2 = b1.next[T]
-    val rec = var0[T ~> T]
-    ap(
-      fix[T ~> T](
-        ???
-        //b.ifLessOrEqualThan(b.int(0))(b.int(1), b.mul(???, ???))
-      )
-    )(n)
+trait LangContext[T, ~>[_, _], E[_]] {
+  def lang: Lang[E[T]]
+  def binding: BindingLang[~>, E]
+  def next[A]: LangContext[A ~> E[T], ~>, λ[X => A ~> E[X]]]
+}
+
+object LangEvaluators {
+  val eval: Lang[Int] = new Lang[Int] {
+    override def int(n: Int): Int = n
+    override def sum(a: Int, b: Int): Int = a + b
+  }
+
+  def lift[T, U](l: Lang[T]): Lang[U => T] = new Lang[U => T] {
+    override def int(n: Int): U => T = _ => l.int(n)
+    override def sum(a: U => T, b: U => T): U => T = u => l.sum(a(u), b(u))
   }
 }
 
-// 2) A simple language for lists, with map and flatmap
+object BindingLangEvaluators {
+  type Id[T] = T
+  val funcEval: BindingLang[Function1, Id] = new BindingLang[Function1, Id] {
+    override def var0[A]: A => Id[A] = identity
+    override def varS[A, B](expr: Id[A]): B => Id[A] = _  => expr
+    override def ap[A, B](f: A => Id[B])(a: A): Id[B] = f(a)
+    override def fix[A](f: A => Id[A]): Id[A] = f(fix(f))
+  }
 
-object Lang {
-  type EnvBase[A] = A
-  type Id[A] = A
+  def lift[E[_], T](bindingLang: BindingLang[Function1, E]): BindingLang[Function1, λ[X => T => E[X]]] =
+    new BindingLang[Function1, λ[X => T => E[X]]] {
+      override def var0[A]: A => (T => E[A]) = varS(bindingLang.var0[A])
+      override def varS[A, B](expr: T => E[A]): B => (T => E[A]) = _ => expr
+      override def ap[A, B](f: A => (T => E[B]))(a: A): T => E[B] = t => bindingLang.ap[A, B](aa => f(aa)(t))(a)
+      override def fix[A](f: A => (T => E[A])): T => E[A] = t => bindingLang.fix[A](aa => f(aa)(t))
+    }
+}
+
+
+
+class IntExpressions[~>[_, _], E[_], T](val langContext: LangContext[T, ~>, E]) {
+  def factorial(n: T): T = ???
 }
