58358eb414efc803d0ca84edc2117c386903fe37
Nicolò Martini <nicolo@martini.io>
2018-07-20 16:44:52 +0100

    Improve tests for BindingAlgebraParser

--- core/shared/src/test/scala/evolution/algebra/primitive/parser/BindingAlgebraParserSpec.scala
+++ core/shared/src/test/scala/evolution/algebra/primitive/parser/BindingAlgebraParserSpec.scala
@@ -12,34 +12,30 @@ class BindingAlgebraParserSpec extends WordSpec with Matchers {
   import fastparse.noApi._
   import evolution.primitive.algebra.parser.PrimitiveParsers._
   "A Binding algebra parser" should {
-    "parse a simple let expression" in {
-      val bindingParser = new BindingAlgebraParser[CtxString](BindingAlgebraSerializer)
+    "parse a simple let expression" in new Fixture {
       val serializedExpression = "let(x, 10.0, $x)"
-      val doubleParser: Parser[CtxString[Double]] = double.map(d => _ => d.toString)
-      val letParser: Parser[CtxString[Double]] = bindingParser.let(doubleParser)
       unsafeParse(serializedExpression, letParser) shouldBe serializedExpression
     }
 
-    "parse a nested let expression" in {
-      val bindingParser = new BindingAlgebraParser[CtxString](BindingAlgebraSerializer)
+    "parse a nested let expression" in new Fixture {
       val serializedExpression = "let(x, let(y, 1.0, $y), let(z, 2.0, $x))"
-      val doubleParser: Parser[CtxString[Double]] = double.map(d => _ => d.toString)
-      def letParser: Parser[CtxString[Double]] =
-        bindingParser.let(doubleParser)
+      bindingParser.let(doubleParser)
       unsafeParse(serializedExpression, letParser) shouldBe serializedExpression
     }
 
-    "parse a double nested let expression" in {
-      val bindingParser = new BindingAlgebraParser[CtxString](BindingAlgebraSerializer)
-      // $x is not recognised
+    "parse a double nested let expression" in new Fixture {
       val serializedExpression = "let(x, let(y, 1.0, $y), let(z, let(u, $x, $u), $x))"
-      val doubleParser: Parser[CtxString[Double]] = double.map(d => _ => d.toString)
-      def letParser: Parser[CtxString[Double]] =
-        bindingParser.let(doubleParser)
       unsafeParse(serializedExpression, letParser) shouldBe serializedExpression
     }
   }
 
   private def unsafeParse[T](expression: String, parser: Parser[CtxString[T]]): String =
     parser.parse(expression).get.value(Nil)
+
+  trait Fixture {
+    val bindingParser = new BindingAlgebraParser[CtxString](BindingAlgebraSerializer)
+    val doubleParser: Parser[CtxString[Double]] = double.map(d => _ => d.toString)
+    val letParser: Parser[CtxString[Double]] =
+      bindingParser.let(doubleParser)
+  }
 }
