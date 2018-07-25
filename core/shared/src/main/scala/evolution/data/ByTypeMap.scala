5e361172051bb717710b202f26e915c437d6c575
Nicolò Martini <nicolo@martini.io>
2018-07-25 20:51:27 +0100

    Use HLists instead of home-made similar constructs in ByType map

--- core/shared/src/main/scala/evolution/data/ByTypeMap.scala
+++ core/shared/src/main/scala/evolution/data/ByTypeMap.scala
@@ -1,30 +1,25 @@
 package evolution.data
 
-import evolution.data.ByTypeMap.With
-
-sealed trait ByTypeMap
+import shapeless._
 
 object ByTypeMap {
-  case object Empty extends ByTypeMap
-  final case class With[H, T <: ByTypeMap](head: H, tail: T) extends ByTypeMap
-
-  implicit def valueInHead[H, T <: ByTypeMap]: HasValue[With[H, T], H] =
-    new HasValue[With[H, T], H] {
-      override def get(t: With[H, T]): H = t.head
-      override def set(t: With[H, T], v: H): With[H, T] = With(v, t.tail)
+  implicit def valueInHead[H, T <: HList]: HasValue[H :: T, H] =
+    new HasValue[H :: T, H] {
+      override def get(t: H :: T): H = t.head
+      override def set(t: H :: T, v: H): H :: T = v :: t.tail
     }
 
-  implicit def valueInTail[H, T <: ByTypeMap, V](implicit tailHasValue: HasValue[T, V]): HasValue[With[H, T], V] =
-    new HasValue[With[H, T], V] {
-      override def get(x: With[H, T]): V = tailHasValue.get(x.tail)
-      override def set(x: With[H, T], v: V): With[H, T] = With(x.head, tailHasValue.set(x.tail, v))
+  implicit def valueInTail[H, T <: HList, V](implicit tailHasValue: HasValue[T, V]): HasValue[H :: T, V] =
+    new HasValue[H :: T, V] {
+      override def get(x: H :: T): V = tailHasValue.get(x.tail)
+      override def set(x: H :: T, v: V): H :: T = x.head :: tailHasValue.set(x.tail, v)
     }
 
-  implicit def hasValueOps[T <: ByTypeMap](t: T): HasValueOps[T] = HasValueOps(t)
-  implicit def byMapOps[T <: ByTypeMap](t: T): ByTypeMapOps[T] = ByTypeMapOps(t)
+  implicit def hasValueOps[T <: HList](t: T): HasValueOps[T] = HasValueOps(t)
+  implicit def byMapOps[T <: HList](t: T): ByTypeMapOps[T] = ByTypeMapOps(t)
 
-  case class ByTypeMapOps[X <: ByTypeMap](x: X) extends AnyVal {
-    def add[H](h: H): With[H, X] = With(h, x)
+  case class ByTypeMapOps[X <: HList](x: X) extends AnyVal {
+    def add[H](h: H): H :: X = h :: x
   }
 
   def get[V] = new GetterHelper[V]
--- core/shared/src/test/scala/evolution/data/ByTypeMapSpec.scala
+++ core/shared/src/test/scala/evolution/data/ByTypeMapSpec.scala
@@ -2,12 +2,13 @@ package evolution.data
 
 import org.scalatest.{Matchers, WordSpec}
 import ByTypeMap._
+import shapeless.HNil
 import shapeless.test.illTyped
 
 class ByTypeMapSpec extends WordSpec with Matchers {
   "A TypeByMap" should {
     "retrieve values by type" in {
-      val byTypeMap = Empty.add("string").add(123).add('a')
+      val byTypeMap = HNil.add("string").add(123).add('a')
       byTypeMap.get[String] shouldBe "string"
       byTypeMap.get[Int] shouldBe 123
       byTypeMap.get[Char] shouldBe 'a'
@@ -15,7 +16,7 @@ class ByTypeMapSpec extends WordSpec with Matchers {
 
     "fail compilation if a value is not found" in {
       illTyped("""get[String](Empty)""")
-      val byTypeMap = With("string", With(123, With('a', Empty)))
+      val byTypeMap = "string" :: 123 :: 'a' :: HNil
       illTyped("""get[Float](byTypeMap)""")
     }
   }
