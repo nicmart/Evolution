83103048dd4d5160f5cdee9d59f482bb9e13dd75
Nicolò Martini <nicolo@martini.io>
2018-07-31 21:55:18 +0100

    ToEvolution interpreter for DrawingAlgebra

--- core/shared/src/main/scala/evolution/primitive/algebra/interpreter/ToEvolution.scala
+++ core/shared/src/main/scala/evolution/primitive/algebra/interpreter/ToEvolution.scala
@@ -1,60 +1,42 @@
 package evolution.primitive.algebra.interpreter
 
-import cats.kernel.Group
-import cats.implicits._
-import evolution.algebra
-import evolution.algebra.{Evolution, EvolutionCoreAlgebra}
+import evolution.algebra.EvolutionCoreAlgebra
 import evolution.primitive.algebra._
 import evolution.geometry.Point
-import evolution.algebra.syntax.all._
 
-//class ToEvolution[F[+ _]](evolutionAlg: EvolutionCoreAlgebra[F]) extends DrawingAlgebra[CtxScalar, CtxF[F, ?]] {
-//  override val drawing: CoreDrawingAlgebra[CtxScalar, CtxF[F, ?]] =
-//    new CoreDrawingAlgebra[CtxScalar, CtxF[F, ?]] {
-//      override def empty[A]: CtxF[F, A] =
-//        _ => evolutionAlg.empty
-//      override def cons[A](head: CtxScalar[A], tail: CtxF[F, A]): CtxF[F, A] =
-//        ctx => evolutionAlg.cons(head(ctx), tail(ctx))
-//      override def mapEmpty[A](eva: CtxF[F, A])(eva2: CtxF[F, A]): CtxF[F, A] =
-//        ctx => evolutionAlg.mapEmpty(eva(ctx))(eva2(ctx))
-//      override def mapCons[A, B](eva: CtxF[F, A])(f: CtxF[F, B]): CtxF[F, B] =
-//        ctx =>
-//          evolutionAlg.mapCons(eva(ctx)) { (a: A, tail: F[A]) =>
-//            f((() => a) :: (() => tail) :: ctx)
-//        }
-//    }
-//  override val scalar: ScalarAlgebra[CtxScalar] = new ScalarAlgebra[CtxScalar] {
-//    override def double(d: Double): CtxScalar[Double] = _ => d
-//    override def point(p: Point): CtxScalar[Point] = _ => p
-//  }
-//  override val bindS: BindingAlgebra[CtxScalar] = new BindingAlgebra[CtxScalar] {
-//    override def var0[A]: CtxScalar[A] = {
-//      case h :: tail => h().asInstanceOf[A]
-//    }
-//    override def shift[A](expr: CtxScalar[A]): CtxScalar[A] = {
-//      case h :: tail => expr(tail)
-//    }
-//    override def let[A, B](name: String, value: CtxScalar[A])(expr: CtxScalar[B]): CtxScalar[B] =
-//      ctx => expr((() => value(ctx)) :: ctx)
-//
-//    override def fix[A](expr: CtxScalar[A]): CtxScalar[A] =
-//      ctx => expr((() => expr(ctx)) :: ctx)
-//
-//    override def lambda[A, B](name: String, expr: CtxScalar[B]): CtxScalar[B] = expr
-//  }
-//  override val bindF: BindingAlgebra[CtxF[F, ?]] = new BindingAlgebra[CtxF[F, ?]] {
-//    override def var0[A]: CtxF[F, A] = {
-//      case h :: tail => h.asInstanceOf[F[A]]
-//    }
-//    override def shift[A](expr: CtxF[F, A]): CtxF[F, A] = {
-//      case h :: tail => expr(tail)
-//    }
-//    override def let[A, B](name: String, value: CtxF[F, A])(expr: CtxF[F, B]): CtxF[F, B] =
-//      ctx => expr((() => value(ctx)) :: ctx)
-//
-//    override def fix[A](expr: CtxF[F, A]): CtxF[F, A] =
-//      ctx => expr((() => expr(ctx)) :: ctx)
-//
-//    override def lambda[A, B](name: String, expr: CtxF[F, B]): CtxF[F, B] = expr
-//  }
-//}
+class ToEvolution[F[+ _]](evolutionAlg: EvolutionCoreAlgebra[F]) extends DrawingAlgebra[Id, F, Ctx] {
+  type CtxF[T] = Ctx[F[T]]
+  override val drawing: CoreDrawingAlgebra[Ctx, CtxF] =
+    new CoreDrawingAlgebra[Ctx, CtxF] {
+      override def empty[A]: Ctx[F[A]] =
+        _ => evolutionAlg.empty
+      override def cons[A](head: Ctx[A], tail: Ctx[F[A]]): Ctx[F[A]] =
+        ctx => evolutionAlg.cons(head(ctx), tail(ctx))
+      override def mapEmpty[A](eva: Ctx[F[A]])(eva2: Ctx[F[A]]): Ctx[F[A]] =
+        ctx => evolutionAlg.mapEmpty(eva(ctx))(eva2(ctx))
+      override def mapCons[A, B](eva: Ctx[F[A]])(f: Ctx[F[B]]): Ctx[F[B]] =
+        ctx =>
+          evolutionAlg.mapCons(eva(ctx)) { (a: A, tail: F[A]) =>
+            f((() => a) :: (() => tail) :: ctx)
+        }
+    }
+  override val scalar: ScalarAlgebra[Ctx] = new ScalarAlgebra[Ctx] {
+    override def double(d: Double): Ctx[Double] = _ => d
+    override def point(p: Point): Ctx[Point] = _ => p
+  }
+  override val bind: BindingAlgebra[Ctx] = new BindingAlgebra[Ctx] {
+    override def var0[A]: Ctx[A] = {
+      case h :: tail => h().asInstanceOf[A]
+    }
+    override def shift[A](expr: Ctx[A]): Ctx[A] = {
+      case h :: tail => expr(tail)
+    }
+    override def let[A, B](name: String, value: Ctx[A])(expr: Ctx[B]): Ctx[B] =
+      ctx => expr((() => value(ctx)) :: ctx)
+
+    override def fix[A](expr: Ctx[A]): Ctx[A] =
+      ctx => expr((() => expr(ctx)) :: ctx)
+
+    override def lambda[A, B](name: String, expr: Ctx[B]): Ctx[B] = expr
+  }
+}
--- core/shared/src/main/scala/evolution/primitive/algebra/interpreter/package.scala
+++ core/shared/src/main/scala/evolution/primitive/algebra/interpreter/package.scala
@@ -7,7 +7,5 @@ package object interpreter {
   type Id[A] = A
   type ConstString[A] = String
   type CtxString[A] = List[String] => String
-  type CtxEvolution[+A] = List[Any] => Evolution[A]
-  type CtxScalar[A] = List[() => Any] => A
-  type CtxF[F[_], A] = List[() => Any] => F[A]
+  type Ctx[A] = List[() => Any] => A
 }
