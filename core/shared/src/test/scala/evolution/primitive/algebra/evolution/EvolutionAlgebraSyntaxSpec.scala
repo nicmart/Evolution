61ba46531861659f6596c4157a5cac250e98d60b
Nicolò Martini <nicolo@martini.io>
2018-10-06 14:54:34 +0100

    Directly use test interpreter to define expected expressions in EvolutionAlgebraSyntaxSpec

--- core/shared/src/test/scala/evolution/primitive/algebra/evolution/EvolutionAlgebraSyntaxSpec.scala
+++ core/shared/src/test/scala/evolution/primitive/algebra/evolution/EvolutionAlgebraSyntaxSpec.scala
@@ -7,28 +7,32 @@ import evolution.primitive.algebra.{ByVarParser, TestInterpreters}
 import evolution.primitive.algebra.evolution.parser.{
   EvolutionAlgebraExpressions,
   EvolutionAlgebraGrammar,
-  EvolutionAlgebraSyntax,
+  EvolutionAlgebraSyntax
 }
 import evolution.primitive.parser.CommonTestParsers
 import org.scalatest.{FreeSpec, Matchers}
 import evolution.primitive.algebra.parser.ParserConfig.White._
+import evolution.primitive.algebra.parser.PrimitiveParsers
 import fastparse.noApi
 import fastparse.noApi.{Fail, P, Parser}
 
-class EvolutionAlgebraSyntaxSpec extends FreeSpec with Matchers with CommonTestParsers with TestInterpreters {
+class EvolutionAlgebraSyntaxSpec extends FreeSpec with Matchers with TestInterpreters {
+  val interpreter: EvolutionAlgebra[Constant, ListExpr, Binding, String] = EvolutionAlgebraTestInterpreter
+  import interpreter.bind._, interpreter.constants._, interpreter.list._, interpreter.list.{empty => nil}
+
   "An Evolution Grammar" - {
     "should parse constants" - {
       "simple sum of doubles" in {
         val serializedExpression = "add(1, 2)"
         val expectedExpression =
-          Lift(Add[Double](1, 2))
+          add(double(1), double(2))
         parseConstantOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "nested sum of doubles" in {
         val serializedExpression = "add(1, add(2, 3))"
         val expectedExpression =
-          Lift(Add[Double](1, Add[Double](2, 3)))
+          add(double(1), add(double(2), double(3)))
         parseConstantOfDoubles(serializedExpression) shouldBe expectedExpression
       }
     }
@@ -36,73 +40,65 @@ class EvolutionAlgebraSyntaxSpec extends FreeSpec with Matchers with CommonTestP
     "should parse" - {
       "an empty evolution of Doubles" in {
         val serializedExpression = "empty"
-        val expectedExpression = Lift(Empty[Double]())
+        val expectedExpression = nil[Double]
         parseEvolutionOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "an empty evolution of Points" in {
         val serializedExpression = "empty"
-        val expectedExpression = Lift(Empty[Point]())
+        val expectedExpression = nil[Point]
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution with a single Double" in {
         val serializedExpression = "cons(1, empty)"
-        val expectedExpression = Lift(Cons(1.0, Empty[Double]()))
+        val expectedExpression = cons(double(1), nil)
         parseEvolutionOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution with a single point" in {
         val serializedExpression = "cons(point(0, 1), empty)"
-        val expectedExpression = Lift(Cons(PointConstant(0.0, 1.0), Empty[Point]()))
+        val expectedExpression = cons(point(double(0), double(1)), nil)
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution with two doubles" in {
         val serializedExpression = "cons(1, cons(2, empty))"
-        val expectedExpression = Lift(Cons(1.0, Cons(2.0, Empty[Double]())))
+        val expectedExpression = cons(double(1), cons(double(2), nil))
         parseEvolutionOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution with two points" in {
         val serializedExpression = "cons(point(0, 0), cons(point(1, 1), empty))"
-        val expectedExpression = Lift(Cons(PointConstant(0, 0), Cons(PointConstant(1, 1), Empty[Point]())))
+        val expectedExpression = cons(point(double(0), double(0)), cons(point(double(1), double(1)), nil))
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution where there are sums of numbers" in {
         val serializedExpression = "cons(add(1, 2), cons(add(3, add(4, 5)), empty))"
         val expectedExpression =
-          Lift(Cons[Double](Add[Double](1, 2), Cons[Double](Add[Double](3, Add[Double](4, 5)), Empty[Double]())))
+          cons(add(double(1), double(2)), cons(add(double(3), add(double(4), double(5))), nil))
         parseEvolutionOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution where there are sums of points" in {
         val serializedExpression = "cons(add(point(1, 1), point(3, 4)), empty)"
         val expectedExpression =
-          Lift(Cons[Point](Add[Point](PointConstant(1, 1), PointConstant(3, 4)), Empty[Point]()))
+          cons(add(point(double(1), double(1)), point(double(3), double(4))), nil)
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution where there are sum of doubles inside point coordinates" in {
         val serializedExpression = "cons(point(add(1, 2), 3), empty)"
         val expectedExpression =
-          Lift(Cons[Point](PointConstant(Add(1.0, 2.0), 3.0), Empty[Point]()))
+          cons(point(add(double(1), double(2)), double(3)), nil)
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution with a mapCons" in {
         val serializedExpression = "mapCons(empty, lambda(head)(lambda(tail)(empty)))"
         val expectedExpression =
-          Lift(
-            MapCons(
-              Empty[Double](),
-              Lambda[Constant[Double], ListExpr[Double] => ListExpr[Double]](
-                "head",
-                Lambda[ListExpr[Double], ListExpr[Double]]("tail", Lift(Empty[Double]()))
-              )
-            )
-          )
+          mapCons[Double, Double](nil)(lambda("head", lambda("tail", nil)))
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
     }
@@ -156,7 +152,7 @@ class EvolutionAlgebraSyntaxSpec extends FreeSpec with Matchers with CommonTestP
       self,
       syntax,
       syntax.doubleConstant,
-      varName,
+      PrimitiveParsers.varName,
       orMonoid
     )
 
