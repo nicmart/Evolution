29af98f519d1f0ba9c1f75ba0b559e8657f6c218
Nicolò Martini <nicolo@martini.io>
2018-10-06 15:29:37 +0100

    Directly use test interpreter to define expected expressions in ConstantsAlgebraSyntaxSpec

--- core/shared/src/test/scala/evolution/primitive/algebra/constants/ConstantsAlgebraSyntaxSpec.scala
+++ core/shared/src/test/scala/evolution/primitive/algebra/constants/ConstantsAlgebraSyntaxSpec.scala
@@ -1,42 +1,44 @@
 package evolution.primitive.algebra.constants
 
 import cats.implicits._
-import cats.kernel.Semigroup
 import cats.{Defer, MonoidK}
-import evolution.primitive.parser.CommonTestParsers
 import evolution.geometry.Point
 import evolution.primitive.algebra.{Composed, TestInterpreters}
 import evolution.primitive.algebra.constants.parser._
 import evolution.primitive.algebra.parser.ParserConfig.White._
 import fastparse.noApi.{Fail, P, Parser}
 import org.scalatest.{FreeSpec, Matchers}
+import evolution.primitive.algebra.parser.PrimitiveParsers.doubleLiteral
 
-class ConstantsAlgebraSyntaxSpec extends FreeSpec with Matchers with CommonTestParsers with TestInterpreters {
+class ConstantsAlgebraSyntaxSpec extends FreeSpec with Matchers with TestInterpreters {
+  val interpreter: ConstantsAlgebra[Composed[Binding, Constant, ?]] =
+    ConstantsAlgebraTestInterpreter
+  import interpreter._
 
   "A ScalarAlgebraParser should parse" - {
     "double literals" in {
       val serializedExpression = "1.0"
-      unsafeParseDouble(serializedExpression) shouldBe Value(1.0)
+      unsafeParseDouble(serializedExpression) shouldBe double(1)
     }
 
     "negative double literals" in {
       val serializedExpression = "-2.0"
-      unsafeParseDouble(serializedExpression) shouldBe Value(-2.0)
+      unsafeParseDouble(serializedExpression) shouldBe double(-2)
     }
 
     "point literals" in {
       val serializedExpression = "point(1.0, -2.51)"
-      unsafeParsePoint(serializedExpression) shouldBe Value(Point(1.0, -2.51))
+      unsafeParsePoint(serializedExpression) shouldBe point(double(1.0), double(-2.51))
     }
 
     "sum of doubles" in {
       val serializedExpression = "add(1.0, 2.0)"
-      unsafeParseDouble(serializedExpression) shouldBe Add(1.0, 2.0)
+      unsafeParseDouble(serializedExpression) shouldBe add(double(1.0), double(2.0))
     }
 
     "sum of points" in {
       val serializedExpression = "add(point(1.0, 2.0), point(3, 4))"
-      unsafeParsePoint(serializedExpression) shouldBe Add(Point(1.0, 2.0), Point(3.0, 4.0))
+      unsafeParsePoint(serializedExpression) shouldBe add(point(double(1), double(2)), point(double(3), double(4)))
     }
   }
 
@@ -47,9 +49,6 @@ class ConstantsAlgebraSyntaxSpec extends FreeSpec with Matchers with CommonTestP
     doubleLiteral.map { d =>
       Lift(Value(d))
     }
-
-  val pointParser: Parser[Binding[Constant[Point]]] =
-    function2("point", doubleLiteral, doubleLiteral).map { case (x, y) => Lift(Value(Point(x, y))) }
 
   // TODO move somewhere
   lazy val parserMonoidK: MonoidK[ConstantParser] = new MonoidK[ConstantParser] {
@@ -62,7 +61,7 @@ class ConstantsAlgebraSyntaxSpec extends FreeSpec with Matchers with CommonTestP
   }
 
   val syntax: ConstantsAlgebraSyntax[Composed[Binding, Constant, ?]] =
-    new ConstantsAlgebraSyntax[Composed[Binding, Constant, ?]](ConstantsAlgebraTestInterpreter)
+    new ConstantsAlgebraSyntax[Composed[Binding, Constant, ?]](interpreter)
 
   def grammar(self: Expressions[ConstantParser]): Expressions[ConstantParser] =
     new ConstantsAlgebraGrammar[ConstantParser](self, syntax, parserMonoidK)
@@ -70,12 +69,9 @@ class ConstantsAlgebraSyntaxSpec extends FreeSpec with Matchers with CommonTestP
   def expressions: Expressions[ConstantParser] =
     grammar(new LazyExpressions[ConstantParser](expressions, deferParser))
 
-  private def unsafeParseDouble(serializedExpression: String): Constant[Double] =
-    expressions.get(doubleParser).parse(serializedExpression).get.value match {
-      case Lift(inner) => inner
-    }
-  private def unsafeParsePoint(serializedExpression: String): Constant[Point] =
-    expressions.get(pointParser).parse(serializedExpression).get.value match {
-      case Lift(inner) => inner
-    }
+  private def unsafeParseDouble(serializedExpression: String): Binding[Constant[Double]] =
+    expressions.get(doubleParser).parse(serializedExpression).get.value
+
+  private def unsafeParsePoint(serializedExpression: String): Binding[Constant[Point]] =
+    expressions.get(syntax.anyPoint).parse(serializedExpression).get.value
 }
