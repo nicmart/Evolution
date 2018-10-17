115c7485f6ca9d7027385b265744ba9bf768ee58
Nicolò Martini <nicolo@martini.io>
2018-10-17 19:19:43 +0100

    Parametrise Double in ConstantsApplicative

--- core/shared/src/main/scala/evolution/primitive/algebra/constants/interpreter/ConstantsApplicative.scala
+++ core/shared/src/main/scala/evolution/primitive/algebra/constants/interpreter/ConstantsApplicative.scala
@@ -5,9 +5,8 @@ import evolution.geometry.Point
 import evolution.primitive.algebra.Composed
 import evolution.primitive.algebra.constants.Constants
 
-class ConstantsApplicative[S[_], F[_]: Applicative](alg: Constants[S, Double])
-    extends Constants[Composed[F, S, ?], Double] {
-  override def double(d: Double): F[S[Double]] =
+class ConstantsApplicative[S[_], D, F[_]: Applicative](alg: Constants[S, D]) extends Constants[Composed[F, S, ?], D] {
+  override def double(d: D): F[S[Double]] =
     Applicative[F].pure(alg.double(d))
   override def point(x: F[S[Double]], y: F[S[Double]]): F[S[Point]] =
     Applicative[F].map2(x, y)(alg.point)
