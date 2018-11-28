65fbd374d588b7e81e897597d2b26c1a9d04374f
Nicolò Martini <nicolo@martini.io>
2018-11-28 19:05:33 +0000

    Use TypedSerializer in EvolutionParserSyntaxSpec

--- core/shared/src/test/scala/evolution/primitive/algebra/evolution/parser/EvolutionParserSyntaxSpec.scala
+++ core/shared/src/test/scala/evolution/primitive/algebra/evolution/parser/EvolutionParserSyntaxSpec.scala
@@ -1,16 +1,14 @@
 package evolution.primitive.algebra.evolution.parser
 
-import cats.implicits._
-import cats.kernel.Semigroup
 import evolution.geometry.Point
-import evolution.primitive.algebra.TestInterpreters
-import evolution.primitive.algebra.evolution.Evolution
+import evolution.primitive.algebra.evolution.interpreter.Types._
+import evolution.primitive.algebra.evolution.interpreter.{ EvolutionTypedSerializer, Types }
 import evolution.primitive.algebra.parser.ByVarParser.ByVarParserK
-import fastparse.noApi.Parser
+import fastparse.noApi
 import org.scalatest.{ FreeSpec, Matchers }
 
-class EvolutionParserSyntaxSpec extends FreeSpec with Matchers with TestInterpreters {
-  val interpreter: Evolution[ListExpr, Binding, String, String] = EvolutionAlgebraTestInterpreter
+class EvolutionParserSyntaxSpec extends FreeSpec with Matchers {
+  val interpreter = new EvolutionTypedSerializer
   import interpreter.bind._
   import interpreter.chain.{ empty => nil, _ }
   import interpreter.constants._
@@ -20,14 +18,14 @@ class EvolutionParserSyntaxSpec extends FreeSpec with Matchers with TestInterpre
       "simple sum of doubles" in {
         val serializedExpression = "add(1, 2)"
         val expectedExpression =
-          add(double(1), double(2))
+          add(double(1), double(2)).infer(doubleConstant).toString
         parseConstantOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "nested sum of doubles" in {
         val serializedExpression = "add(1, add(2, 3))"
         val expectedExpression =
-          add(double(1), add(double(2), double(3)))
+          add(double(1), add(double(2), double(3))).infer(doubleConstant).toString
         parseConstantOfDoubles(serializedExpression) shouldBe expectedExpression
       }
     }
@@ -35,37 +33,38 @@ class EvolutionParserSyntaxSpec extends FreeSpec with Matchers with TestInterpre
     "should parse" - {
       "an empty evolution of Doubles" in {
         val serializedExpression = "empty"
-        val expectedExpression = nil[Double]
+        val expectedExpression = nil[Double].infer(evolutionOfDoubles).toString
         parseEvolutionOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "an empty evolution of Points" in {
         val serializedExpression = "empty"
-        val expectedExpression = nil[Point]
+        val expectedExpression = nil[Point].infer(evolutionOfPoints).toString
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution with a single Double" in {
         val serializedExpression = "cons(1, empty)"
-        val expectedExpression = cons(double(1), nil)
+        val expectedExpression = cons(double(1), nil).infer(evolutionOfDoubles).toString
         parseEvolutionOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution with a single point" in {
         val serializedExpression = "cons(point(0, 1), empty)"
-        val expectedExpression = cons(point(double(0), double(1)), nil)
+        val expectedExpression = cons(point(double(0), double(1)), nil).infer(evolutionOfPoints).toString
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution with two doubles" in {
         val serializedExpression = "cons(1, cons(2, empty))"
-        val expectedExpression = cons(double(1), cons(double(2), nil))
+        val expectedExpression = cons(double(1), cons(double(2), nil)).infer(evolutionOfDoubles).toString
         parseEvolutionOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution with two points" in {
         val serializedExpression = "cons(point(0, 0), cons(point(1, 1), empty))"
-        val expectedExpression = cons(point(double(0), double(0)), cons(point(double(1), double(1)), nil))
+        val expectedExpression =
+          cons(point(double(0), double(0)), cons(point(double(1), double(1)), nil)).infer(evolutionOfPoints).toString
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
@@ -73,48 +72,50 @@ class EvolutionParserSyntaxSpec extends FreeSpec with Matchers with TestInterpre
         val serializedExpression = "cons(add(1, 2), cons(add(3, add(4, 5)), empty))"
         val expectedExpression =
           cons(add(double(1), double(2)), cons(add(double(3), add(double(4), double(5))), nil))
+            .infer(evolutionOfDoubles)
+            .toString
         parseEvolutionOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution where there are sums of points" in {
         val serializedExpression = "cons(add(point(1, 1), point(3, 4)), empty)"
         val expectedExpression =
-          cons(add(point(double(1), double(1)), point(double(3), double(4))), nil)
+          cons(add(point(double(1), double(1)), point(double(3), double(4))), nil).infer(evolutionOfPoints).toString
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution where there are sum of doubles inside point coordinates" in {
         val serializedExpression = "cons(point(add(1, 2), 3), empty)"
         val expectedExpression =
-          cons(point(add(double(1), double(2)), double(3)), nil)
+          cons(point(add(double(1), double(2)), double(3)), nil).infer(evolutionOfPoints).toString
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution with a mapCons" in {
         val serializedExpression = "mapCons(empty, head -> tail -> empty)"
         val expectedExpression =
-          mapCons[Double, Double](nil)(lambda("head", lambda("tail", nil)))
-        parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
+          mapCons[Double, Double](nil)(lambda("head", lambda("tail", nil))).infer(evolutionOfDoubles).toString
+        parseEvolutionOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "a constant evolution defined as a fixed point" in {
         val serializedExpression = "fix(s -> $s)"
-        val expectedExpression: Binding[ListExpr[Double]] =
-          fix(lambda("s", var0[ListExpr[Double]]))
+        val expectedExpression: String =
+          fix[F[Double]](lambda("s", var0[F[Double]])).infer(evolutionOfDoubles).toString
         parseEvolutionOfDoubles(serializedExpression) shouldBe expectedExpression
       }
 
       "a lambda from an evolution to another" in {
         val serializedExpression = "s -> $s"
-        val expectedExpression: Binding[ListExpr[Double] => ListExpr[Double]] =
-          lambda("s", var0[ListExpr[Double]])
+        val expectedExpression: String =
+          lambda("s", var0[F[Double]]).infer(lambdaTypeInfo).toString
         parseLambdaOfEvolutions(serializedExpression) shouldBe expectedExpression
       }
 
       "an evolution expressed as a variable" in {
         val serializedExpression = "$s"
-        val expectedExpression: Binding[ListExpr[Double]] =
-          var0[ListExpr[Double]]
+        val expectedExpression: String =
+          var0[F[Double]].infer(evolutionOfDoubles).toString
         parseEvolutionOfDoubles(serializedExpression, "s" :: Nil) shouldBe expectedExpression
       }
 
@@ -131,8 +132,8 @@ class EvolutionParserSyntaxSpec extends FreeSpec with Matchers with TestInterpre
        * */
       "an ambiguous evolution" in {
         val serializedExpression = "app(fix(f->$f),point(0,0))"
-        val expectedExpression: Binding[Point] =
-          app[Point, Point](fix(lambda("f", var0)), point(double(0), double(0)))
+        val expectedExpression: String =
+          app[Point, Point](fix(lambda("f", var0)), point(double(0), double(0))).infer(pointConstant).toString
         parseConstantOfPoints(serializedExpression) shouldBe expectedExpression
       }
 
@@ -146,39 +147,64 @@ class EvolutionParserSyntaxSpec extends FreeSpec with Matchers with TestInterpre
         pending
         val serializedExpression =
           "app(app(fix(self->start->evolution->mapCons($evolution,h->t->cons($start,app(app($self,add($start,$h)),$t)))),point(1.0,1.0)),fix(self->cons(point(1.0,1.0),$self)))"
-        val expectedExpression: Binding[ListExpr[Double]] =
-          var0[ListExpr[Double]]
+        val expectedExpression = ""
         parseEvolutionOfPoints(serializedExpression) shouldBe expectedExpression
       }
     }
   }
 
-  def parseLambdaOfEvolutions(serializedExpression: String): Binding[ListExpr[Double] => ListExpr[Double]] =
+  lazy val lambdaTypeInfo = FunctionTypeInfo(doubleConstant, doubleConstant)
+
+  def parseLambdaOfEvolutions(serializedExpression: String): String =
     expressions
       .function(expressions.evolutionOfDoubles, expressions.evolutionOfDoubles)
       .parser(Nil)
       .parse(serializedExpression)
       .get
       .value
+      .infer(lambdaTypeInfo)
+      .toString
 
   def parseEvolutionOfDoubles(
     serializedExpression: String,
     currentVars: List[String] = Nil
-  ): Binding[ListExpr[Double]] =
-    expressions.evolutionOfDoubles.parser(currentVars).parse(serializedExpression).get.value
+  ): String =
+    expressions.evolutionOfDoubles
+      .parser(currentVars)
+      .parse(serializedExpression)
+      .get
+      .value
+      .infer(evolutionOfDoubles)
+      .toString
 
-  def parseEvolutionOfPoints(serializedExpression: String, currentVars: List[String] = Nil): Binding[ListExpr[Point]] =
-    expressions.evolutionOfPoints.parser(currentVars).parse(serializedExpression).get.value
+  def parseEvolutionOfPoints(serializedExpression: String, currentVars: List[String] = Nil): String =
+    expressions.evolutionOfPoints
+      .parser(currentVars)
+      .parse(serializedExpression)
+      .get
+      .value
+      .infer(evolutionOfPoints)
+      .toString
 
-  def parseConstantOfDoubles(serializedExpression: String, currentVars: List[String] = Nil): Binding[Double] = {
-    expressions.doubleConstant.loggingParser(currentVars).parse(serializedExpression).get.value
+  def parseConstantOfDoubles(serializedExpression: String, currentVars: List[String] = Nil): String = {
+    expressions.doubleConstant
+      .loggingParser(currentVars)
+      .parse(serializedExpression)
+      .get
+      .value
+      .infer(doubleConstant)
+      .toString
   }
 
-  def parseConstantOfPoints(serializedExpression: String, currentVars: List[String] = Nil): Binding[Point] =
-    expressions.pointConstant.loggingParser(currentVars).parse(serializedExpression).get.value
+  def parseConstantOfPoints(serializedExpression: String, currentVars: List[String] = Nil): String =
+    expressions.pointConstant
+      .loggingParser(currentVars)
+      .parse(serializedExpression)
+      .get
+      .value
+      .infer(pointConstant)
+      .toString
 
-  type BindingParser[T] = ByVarParserK[Binding, T]
-
-  val expressions: Expressions[ListExpr, BindingParser, Parser[String]] =
-    EvolutionGrammar.parserGrammar(EvolutionAlgebraTestInterpreter)
+  val expressions: Expressions[F, ByVarParserK[R, ?], noApi.Parser[String]] =
+    EvolutionGrammar.parserGrammar(interpreter)
 }
