ff180a2b243e8cc02a3921f5bf7f356b83b67925
Nicolò Martini <nicolo@martini.io>
2018-12-16 21:09:07 +0000

    Annotation evaluations

--- core/shared/src/main/scala/evolution/data/annotation.scala
+++ core/shared/src/main/scala/evolution/data/annotation.scala
@@ -13,11 +13,7 @@ case class Annotation[T](vars: Set[Int], info: Annotation.Info[T]) {
   def maxVar: Int = vars.max
   def isClosed: Boolean = vars.isEmpty
   def isOpen: Boolean = !isClosed
-  def evaluate(evaluator: Evolution[RNGRepr, Evaluation], ctx: Ctx): T = info match {
-    case Info.Fix(Annotation(_, Info.ConstantLambda(term, _)), _) =>
-      unshiftExpr(term).run(evaluator).evaluateWith(ctx)
-    case Info.Unknown(expr) => expr.run(evaluator).evaluateWith(ctx)
-  }
+  def evaluate(evaluator: Evolution[RNGRepr, Evaluation], ctx: Ctx): T = Annotation.evaluate(this, evaluator, ctx)
   def expr: Expr[RNGRepr, T] = info.expr
 }
 
@@ -25,10 +21,20 @@ object Annotation {
   sealed trait Info[T] {
     def expr: Expr[RNGRepr, T]
   }
+
   object Info {
     final case class Unknown[T](expr: Expr[RNGRepr, T]) extends Info[T]
     final case class Fix[T](inner: Annotation[T => T], expr: Expr[RNGRepr, T]) extends Info[T]
     final case class ConstantLambda[A, B](term: Expr[RNGRepr, B], expr: Expr[RNGRepr, A => B]) extends Info[A => B]
     final case class StatelessEvolution[A](expr: Expr[RNGRepr, RNGRepr[A]]) extends Info[RNGRepr[A]]
   }
+
+  def evaluate[T](annotation: Annotation[T], evaluator: Evolution[RNGRepr, Evaluation], ctx: Ctx): T =
+    annotation.info match {
+      case Info.Fix(Annotation(_, Info.ConstantLambda(term, _)), _) =>
+        unshiftExpr(term).run(evaluator).evaluateWith(ctx)
+      case Info.Unknown(expr)              => expr.run(evaluator).evaluateWith(ctx)
+      case Info.ConstantLambda(term, expr) => expr.run(evaluator).evaluateWith(ctx)
+      case Info.StatelessEvolution(expr)   => expr.run(evaluator).evaluateWith(ctx)
+    }
 }
