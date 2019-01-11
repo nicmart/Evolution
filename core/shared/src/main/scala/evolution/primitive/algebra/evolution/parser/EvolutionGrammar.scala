9642f0c06a93564d9164014db086254a19720fb2
Nicolò Martini <nicolo@martini.io>
2019-01-11 16:32:10 +0000

    Add boolean evolutions to evolution grammar

--- core/shared/src/main/scala/evolution/primitive/algebra/evolution/parser/EvolutionGrammar.scala
+++ core/shared/src/main/scala/evolution/primitive/algebra/evolution/parser/EvolutionGrammar.scala
@@ -16,8 +16,10 @@ trait Expressions[F[_], R[_]] {
   def pointConstant: R[Point]
   def intConstant: R[Int]
   def boolConstant: R[Boolean]
+
   def evolutionOfDoubles: R[F[Double]]
   def evolutionOfPoints: R[F[Point]]
+  def evolutionOfBools: R[F[Boolean]]
   def function[T1, T2](t1: R[T1], t2: R[T2]): R[T1 => T2]
 }
 
@@ -28,8 +30,10 @@ object Expressions {
     def pointConstant: R[Point] = defer.defer(inner.pointConstant)
     def intConstant: R[Int] = defer.defer(inner.intConstant)
     def boolConstant: R[Boolean] = defer.defer(inner.boolConstant)
+
     def evolutionOfDoubles: R[F[Double]] = defer.defer(inner.evolutionOfDoubles)
     def evolutionOfPoints: R[F[Point]] = defer.defer(inner.evolutionOfPoints)
+    def evolutionOfBools: R[F[Boolean]] = defer.defer(inner.evolutionOfBools)
     def function[T1, T2](t1: R[T1], t2: R[T2]): R[T1 => T2] = defer.defer(inner.function(t1, t2))
   }
 }
@@ -74,31 +78,41 @@ class EvolutionGrammar[F[_], R[_]](syntax: EvolutionSyntax[F, R], override val o
   override def evolutionOfDoubles: R[F[Double]] =
     or(
       distribution.uniform(doubleConstant, doubleConstant),
-      genericEvolution(self.doubleConstant, self.evolutionOfDoubles)
+      genericVectorEvolution(self.doubleConstant, self.evolutionOfDoubles)
     )
 
   override def evolutionOfPoints: R[F[Point]] =
     or(
       derived.cartesian(self.evolutionOfDoubles, self.evolutionOfDoubles),
       derived.polar(self.evolutionOfDoubles, self.evolutionOfDoubles),
-      genericEvolution(self.pointConstant, self.evolutionOfPoints)
+      genericVectorEvolution(self.pointConstant, self.evolutionOfPoints)
+    )
+
+  override def evolutionOfBools: R[F[Boolean]] =
+    or(
+      genericEvolution(self.boolConstant, self.evolutionOfBools)
     )
 
   private def genericVectorConstant[T: VectorSpace](t: R[T]): R[T] =
     or(constants.add(t, t), constants.inverse(t), constants.multiply(self.doubleConstant, t), genericExpr(t))
 
-  private def genericEvolution[T: VectorSpace](t: R[T], ft: R[F[T]]): R[F[T]] =
+  private def genericEvolution[T](t: R[T], ft: R[F[T]]): R[F[T]] =
     or(
       chain.empty,
       chain.cons(t, ft),
       allMapConsedEvolutions(ft),
       chain.mapEmpty(ft, ft),
       derived.constant(t),
-      derived.integrate(t, ft),
       derived.concat(ft, ft),
       allMappedEvolutions(t),
       allFlatMappedEvolutions(ft),
       genericExpr(ft)
+    )
+
+  private def genericVectorEvolution[T: VectorSpace](t: R[T], ft: R[F[T]]): R[F[T]] =
+    or(
+      derived.integrate(t, ft),
+      genericEvolution(t, ft)
     )
 
   private def genericExpr[T](t: R[T]): R[T] =
@@ -112,36 +126,44 @@ class EvolutionGrammar[F[_], R[_]](syntax: EvolutionSyntax[F, R], override val o
   private def allMapConsedEvolutions[T](ft: R[F[T]]): R[F[T]] =
     or(
       chain.mapCons(self.evolutionOfDoubles)(function(self.doubleConstant, function(self.evolutionOfDoubles, ft))),
-      chain.mapCons(self.evolutionOfPoints)(function(self.pointConstant, function(self.evolutionOfPoints, ft)))
+      chain.mapCons(self.evolutionOfPoints)(function(self.pointConstant, function(self.evolutionOfPoints, ft))),
+      chain.mapCons(self.evolutionOfBools)(function(self.boolConstant, function(self.evolutionOfBools, ft)))
     )
 
   private def allMappedEvolutions[T](t: R[T]): R[F[T]] =
     or(
       derived.map(self.evolutionOfDoubles, function(self.doubleConstant, t)),
-      derived.map(self.evolutionOfPoints, function(self.pointConstant, t))
+      derived.map(self.evolutionOfPoints, function(self.pointConstant, t)),
+      derived.map(self.evolutionOfBools, function(self.boolConstant, t))
     )
 
   private def allFlatMappedEvolutions[T](ft: R[F[T]]): R[F[T]] =
     or(
       derived.flatMap(self.evolutionOfDoubles, function(self.doubleConstant, ft)),
-      derived.flatMap(self.evolutionOfPoints, function(self.pointConstant, ft))
+      derived.flatMap(self.evolutionOfPoints, function(self.pointConstant, ft)),
+      derived.flatMap(self.evolutionOfBools, function(self.boolConstant, ft))
     )
 
   private def allLetExpressions[T](t: R[T]): R[T] =
     or(
       bind.let(bind.allVars, self.doubleConstant, t),
       bind.let(bind.allVars, self.pointConstant, t),
+      bind.let(bind.allVars, self.boolConstant, t),
+      bind.let(bind.allVars, self.intConstant, t),
       bind.let(bind.allVars, self.evolutionOfDoubles, t),
-      bind.let(bind.allVars, self.evolutionOfPoints, t)
+      bind.let(bind.allVars, self.evolutionOfPoints, t),
+      bind.let(bind.allVars, self.evolutionOfPoints, t),
     )
 
   private def allAppExpressions[T](t: R[T]): R[T] =
     or(
       bind.app(self.function(self.doubleConstant, t), self.doubleConstant),
       bind.app(self.function(self.pointConstant, t), self.pointConstant),
+      bind.app(self.function(self.boolConstant, t), self.boolConstant),
       bind.app(self.function(self.intConstant, t), self.intConstant),
       bind.app(self.function(self.evolutionOfDoubles, t), self.evolutionOfDoubles),
-      bind.app(self.function(self.evolutionOfPoints, t), self.evolutionOfPoints)
+      bind.app(self.function(self.evolutionOfPoints, t), self.evolutionOfPoints),
+      bind.app(self.function(self.evolutionOfBools, t), self.evolutionOfBools)
     )
 
   private case class Def[T](constant: R[T], evolution: R[F[T]])
