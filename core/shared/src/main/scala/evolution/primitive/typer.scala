1b67db0ed0409d801ad755784e3112eafbec17fe
Nicolò Martini <nicolo@martini.io>
2019-01-20 11:30:33 +0000

    Typecheck integers and doubles

--- core/shared/src/main/scala/evolution/primitive/typer.scala
+++ core/shared/src/main/scala/evolution/primitive/typer.scala
@@ -1,9 +1,30 @@
 package evolution.primitive
 import evolution.primitive.Ast
 
-object typer {
-  class Typer[F[_]](ast: Ast[F]) {
-    import ast._
-    def check(ctx: Context, expected: Type, expr: Expr) = ???
-  }
+import scala.util.Try
+
+class Typer[F[_]](val ast: Ast[F]) {
+  import ast._
+  def check(ctx: Context, expected: Type, expr: Expr): Either[String, Expr] =
+    (expr, expected) match {
+      case (Expr.Var(name, tpe), _) =>
+        Left("")
+      case (Expr.FuncCall(funcName, args, tpe), _) =>
+        Left("")
+      case (Expr.BinaryOp(op, a, b, tpe), _) =>
+        Left("")
+      case (Expr.Lambda(varName, body, tpe), _) =>
+        Left("")
+
+      case (Expr.Number(n, tpe), Type.Dbl) =>
+        parseDouble(n).map(_ => Expr.Number(n, Typed(Type.Dbl)))
+      case (Expr.Number(n, tpe), Type.Integer) =>
+        parseInt(n).map(_ => Expr.Number(n, Typed(Type.Integer)))
+    }
+
+  private def parseInt(n: String): Either[String, Int] =
+    Try(n.toInt).toEither.left.map(_ => "")
+
+  private def parseDouble(n: String): Either[String, Double] =
+    Try(n.toDouble).toEither.left.map(_ => "")
 }
---
+++ core/shared/src/test/scala/evolution/primitive/TyperSpec.scala
@@ -0,0 +1,45 @@
+package evolution.primitive
+import cats.Id
+import org.scalacheck.Gen
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalatest.{ FreeSpec, Matchers }
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+class TyperSpec extends FreeSpec with Matchers with GeneratorDrivenPropertyChecks {
+  val typer = new Typer[Id](new Ast[Id])
+  import typer.ast._
+
+  "The typer" - {
+    "should successfully type" - {
+      "all numbers as doubles" in {
+        forAll(genNumber) { numberExpr =>
+          val ctx = Context.empty
+          val annotated = typer.check(Context.empty, Type.Dbl, numberExpr).right.get
+          annotated shouldBe numberExpr.copy(tpe = Typed(Type.Dbl))
+        }
+      }
+
+      "integers as int" in {
+        forAll(genIntNumber) { numberExpr =>
+          val ctx = Context.empty
+          val annotated = typer.check(Context.empty, Type.Integer, numberExpr).right.get
+          annotated shouldBe numberExpr.copy(tpe = Typed(Type.Integer))
+        }
+      }
+    }
+
+    "should fail to type" - {
+      "doubles as integers" in {
+        forAll(genNotIntNumber) { numberExpr =>
+          val ctx = Context.empty
+          val annotated = typer.check(Context.empty, Type.Integer, numberExpr)
+          annotated.isLeft shouldBe true
+        }
+      }
+    }
+  }
+
+  def genNumber: Gen[Expr.Number] = arbitrary[Double].map(d => Expr.Number(d.toString))
+  def genIntNumber: Gen[Expr.Number] = arbitrary[Int].map(d => Expr.Number(d.toString))
+  def genNotIntNumber: Gen[Expr.Number] = arbitrary[Int].map(d => Expr.Number((0.1 + d).toString))
+}
