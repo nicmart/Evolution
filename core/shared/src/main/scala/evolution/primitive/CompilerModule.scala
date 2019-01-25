84aae26cacd00aa8530359b2190a3f44ebe9fed4
Nicolò Martini <nicolo@martini.io>
2019-01-25 13:49:04 +0000

    Compile Lambdas

--- core/shared/src/main/scala/evolution/primitive/CompilerModule.scala
+++ core/shared/src/main/scala/evolution/primitive/CompilerModule.scala
@@ -1,21 +1,36 @@
 package evolution.primitive
 import evolution.primitive.algebra.evolution.Evolution
+import cats.implicits._
 
 trait CompilerModule[F[_]] { self: WithAst[F] =>
   import ast._
 
   object Compiler {
-    def compile[R[_]](expr: Expr, alg: Evolution[F, R]): Either[String, R[expr.Out]] =
-      expr match {
-        case Expr.Var(name, tpe) =>
-          Right(alg.bind.var0[expr.Out](name))
-        case Expr.FuncCall(funcId, args, tpe)  => ???
-        case Expr.Lambda(varName, body, tpe)   => ???
-        case Expr.Let(varName, value, in, tpe) => ???
-        case Expr.Number(n, Type.Dbl) =>
-          Right(alg.constants.double(n.toDouble).asInstanceOf[R[expr.Out]])
-        case _ =>
-          Left("Invalid type")
-      }
+    def compile[R[_]](expr: Expr, alg: Evolution[F, R]): Either[String, R[expr.Out]] = {
+      def compile(expr: Expr): Either[String, R[expr.Out]] = {
+        expr match {
+          case Expr.Var(name, tpe) =>
+            Right(alg.bind.var0[expr.Out](name))
+          case fc @ Expr.FuncCall(funcId, args, tpe) =>
+            compileFuncCall(fc)
+          case Expr.Lambda(varName, body, tpe) =>
+            compile(body).map(alg.bind.lambda(varName.name, _))
+          case Expr.Let(varName, value, in, tpe) =>
+            for {
+              compiledValue <- compile(value)
+              compiledIn <- compile(in)
+            } yield alg.bind.let(varName.name, compiledValue, compiledIn)
+          case Expr.Number(n, Type.Dbl) =>
+            Right(alg.constants.double(n.toDouble))
+          case _ =>
+            Left("Invalid type")
+        }
+      }.asInstanceOf[Either[String, R[expr.Out]]]
+
+      def compileFuncCall(f: Expr.FuncCall): Either[String, R[f.Out]] = ???
+
+      compile(expr)
+    }
   }
+
 }
--- core/shared/src/test/scala/evolution/primitive/CompilerSpec.scala
+++ core/shared/src/test/scala/evolution/primitive/CompilerSpec.scala
@@ -16,6 +16,18 @@ class CompilerSpec extends CompilerSpecModule[initial.F] {
       "variable usages" in forAll(genTypedVar) { v =>
         unsafeCompile(v) shouldBe initial.Var0[v.Out](v.name)
       }
+
+      "let bindings" in forAll(genTypedVar, genTypedNumber, genTypedNumber) { (variable, n1, n2) =>
+        val variable1 = Expr.Var(variable.name, n1.tpe)
+        unsafeCompile(Expr.Let(variable1, n1, n2)) shouldBe initial.Let(
+          variable1.name,
+          unsafeCompile(n1),
+          unsafeCompile(n2))
+      }
+
+      "lambdas" in forAll(genTypedVar, genTypedNumber) { (variable, n) =>
+        unsafeCompile(Lambda(variable, n)) shouldBe initial.Lambda(variable.name, unsafeCompile(n))
+      }
     }
   }
 
