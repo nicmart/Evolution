e7eb94b22772eb52291468a13ba10f867c86ca96
Nicolò Martini <nicolo@martini.io>
2019-01-26 16:37:29 +0000

    Improve parsers with Cuts

--- core/shared/src/main/scala/evolution/primitive/ParsersModule.scala
+++ core/shared/src/main/scala/evolution/primitive/ParsersModule.scala
@@ -8,26 +8,23 @@ trait ParsersModule[F[_]] { self: WithAst[F] =>
 
   object Parsers {
     lazy val parser: Parser[Expr] =
-      expr0 ~ End
+      P(precedence0 ~ End)
 
-    lazy val expr0: Parser[Expr] =
-      P(lambda | expr1)
+    lazy val precedence0: Parser[Expr] =
+      P(lambda | precedence1)
 
-    lazy val expr1: Parser[Expr] =
-      P(infix(expr2, ops0, expr1) | expr2)
+    lazy val precedence1: Parser[Expr] =
+      P(precedence2 ~ ("+" ~/ precedence2).rep).map {
+        case (head, tail) => evalAssocBinaryOp(head, tail.toList, PredefinedFunction.Add)
+      }
 
-    lazy val expr2: Parser[Expr] =
-      P(unaryPrefixOp | infix(expr3, ops1, expr2) | expr3)
+    lazy val precedence2: Parser[Expr] =
+      P(factor ~ ("*" ~/ factor).rep).map {
+        case (head, tail) => evalAssocBinaryOp(head, tail.toList, PredefinedFunction.Multiply)
+      }
 
-    lazy val expr3: Parser[Expr] =
-      P(("(" ~ expr0 ~ ")") | number | variable | let | funcCall)
-
-    def infix(a: Parser[Expr], op: Parser[PredefinedFunction], b: Parser[Expr]): Parser[Expr] =
-      P(a ~ op ~ b).map { case (a, op, b) => Expr.FuncCall(op, List(a, b)) }
-
-    // Operators in order of precedence
-    lazy val ops0: Parser[PredefinedFunction] = P("+").map(_ => PredefinedFunction.Add)
-    lazy val ops1: Parser[PredefinedFunction] = P("*").map(_ => PredefinedFunction.Multiply)
+    lazy val factor: Parser[Expr] =
+      P(("(" ~ precedence0 ~ ")") | number | unaryPrefixOp | variable | let | funcCall)
 
     lazy val number: Parser[Expr.Number] =
       numbers.doubleLiteral.map(Expr.Number(_))
@@ -36,33 +33,38 @@ trait ParsersModule[F[_]] { self: WithAst[F] =>
       P("$" ~~ identifier).map(Expr.Var(_))
 
     lazy val funcCall: Parser[Expr] =
-      func0Call | P(functionName ~/ "(" ~ args ~ ")").map { case (func, args) => Expr.FuncCall(func, args) }
+      func0Call | P(functionName ~ "(" ~/ args ~ ")").map { case (func, args) => Expr.FuncCall(func, args) }
 
     // Functions with 0 arity
     lazy val func0Call: Parser[Expr] =
       P("empty").map(_ => Expr.FuncCall(PredefinedFunction.Empty, Nil))
 
     lazy val unaryPrefixOp: Parser[Expr] =
-      P("-" ~ expr3).map(e => Expr.FuncCall(PredefinedFunction.Inverse, List(e)))
+      P("-" ~ factor).map(e => Expr.FuncCall(PredefinedFunction.Inverse, List(e)))
 
     lazy val lambda: Parser[Expr] =
-      P(identifier ~ "->" ~ expr0).map { case (identifier, body) => Expr.Lambda(Expr.Var(identifier), body) }
+      P(identifier ~ "->" ~/ precedence0).map { case (identifier, body) => Expr.Lambda(Expr.Var(identifier), body) }
 
-    lazy val let: Parser[Expr] = P("let(" ~/ identifier ~ "," ~ expr0 ~ "," ~ expr0 ~ ")").map {
+    lazy val let: Parser[Expr] = P("let(" ~/ identifier ~ "," ~ precedence0 ~ "," ~ precedence0 ~ ")").map {
       case (id, value, in) =>
         Expr.Let(Expr.Var(id), value, in)
     }
 
     lazy val args: Parser[List[Expr]] =
-      P(expr0 ~ ("," ~ args).?).map { case (head, tail) => head :: tail.getOrElse(Nil) }
+      P(precedence0 ~ ("," ~ args).?).map { case (head, tail) => head :: tail.getOrElse(Nil) }
 
     lazy val identifier: Parser[String] = (alpha ~~ alphaNum.repX(1).?).!
-    //lazy val functionName: Parser[PredefinedFunction] = identifier.map(PredefinedFunction.withNameInsensitive).log()
+
     lazy val functionName: Parser[PredefinedFunction] = identifier
       .filter(id => PredefinedFunction.lowerCaseNamesToValuesMap.isDefinedAt(id.toLowerCase))
       .map(PredefinedFunction.withNameInsensitive)
     lazy val alpha: Parser[Unit] = P(CharIn('a' to 'z') | CharIn('A' to 'Z'))
     lazy val alphaNum: Parser[Unit] = P(CharIn('0' to '9') | alpha)
+
+    def evalAssocBinaryOp(head: Expr, tail: List[Expr], op: PredefinedFunction): Expr =
+      (head :: tail).reduceRight[Expr] { (e, m) =>
+        Expr.FuncCall(op, List(e, m))
+      }
 
     object numbers {
 
