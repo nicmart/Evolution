7ca13a3e09e519cd09ac264bacccc5d5d1aaebde
Nicolò Martini <nicolo@martini.io>
2019-02-04 13:07:35 +0000

    Change name of "compiler" for initial encodings to "interpreter"

--- core/shared/src/main/scala/evolution/primitive/InitialInterpreterModule.scala
+++ core/shared/src/main/scala/evolution/primitive/InitialInterpreterModule.scala
@@ -5,28 +5,28 @@ import evolution.data.{ Ctx, Initial }
 import evolution.data.EvaluationContext._
 import evolution.geometry.Point
 
-object InitialCompilerModule {
+object InitialInterpreterModule {
   private val initial: Initial[RNGRepr] = new Initial[RNGRepr] {}
   import initial._
   type Out[T] = Ctx => T
 
-  def compile[T](expr: R[T]): Out[T] = expr match {
+  def interpret[T](expr: R[T]): Out[T] = expr match {
     case Dbl(d)                => Out.pure(d)
-    case Floor(d)              => compile(d).map(_.toInt)
+    case Floor(d)              => interpret(d).map(_.toInt)
     case Integer(n)            => Out.pure(n)
-    case Pnt(x, y)             => compile2(x, y)(Point.apply)
-    case X(p)                  => compile1(p)(_.x)
-    case Y(p)                  => compile1(p)(_.y)
-    case add @ Add(a, b)       => compile2(a, b)(add.semigroup.combine)
-    case Div(a, b)             => compile2(a, b)(_ / _)
-    case Exp(a, b)             => compile2(a, b)(Math.pow)
-    case inv @ Inverse(t)      => compile1(t)(inv.group.inverse)
-    case mult @ Multiply(k, t) => compile2(k, t)(mult.vectorSpace.mult)
-    case Sin(d)                => compile1(d)(Math.sin)
-    case Cos(d)                => compile1(d)(Math.cos)
-    case eq @ Equals(a, b)     => compile2(a, b)(eq.eq.eqv)
+    case Pnt(x, y)             => interpret2(x, y)(Point.apply)
+    case X(p)                  => interpret1(p)(_.x)
+    case Y(p)                  => interpret1(p)(_.y)
+    case add @ Add(a, b)       => interpret2(a, b)(add.semigroup.combine)
+    case Div(a, b)             => interpret2(a, b)(_ / _)
+    case Exp(a, b)             => interpret2(a, b)(Math.pow)
+    case inv @ Inverse(t)      => interpret1(t)(inv.group.inverse)
+    case mult @ Multiply(k, t) => interpret2(k, t)(mult.vectorSpace.mult)
+    case Sin(d)                => interpret1(d)(Math.sin)
+    case Cos(d)                => interpret1(d)(Math.cos)
+    case eq @ Equals(a, b)     => interpret2(a, b)(eq.eq.eqv)
     case IfThen(condition, a, b) =>
-      compile3(condition, a, b) { (compiledCondition, compiledA, compiledB) =>
+      interpret3(condition, a, b) { (compiledCondition, compiledA, compiledB) =>
         if (compiledCondition) compiledA else compiledB
       }
 
@@ -36,22 +36,22 @@ object InitialCompilerModule {
 
     case Shift(e) =>
       ctx =>
-        compile(e)(pop(ctx))
+        interpret(e)(pop(ctx))
 
     case Let(_, value, e) =>
       ctx =>
-        compile(e)(pushStrict(compile(value)(ctx), ctx, ""))
+        interpret(e)(pushStrict(interpret(value)(ctx), ctx, ""))
 
     case Lambda(_, e) =>
       ctx => a =>
-        compile(e)(pushStrict(a, ctx, ""))
+        interpret(e)(pushStrict(a, ctx, ""))
 
-    case App(f, a) => compile2(f, a)(_(_))
+    case App(f, a) => interpret2(f, a)(_(_))
 
     case Fix(Lambda(_, lambdaBody)) =>
       ctx =>
         {
-          lazy val a: T = compile(lambdaBody)(pushLazy(() => a, ctx, ""))
+          lazy val a: T = interpret(lambdaBody)(pushLazy(() => a, ctx, ""))
           a
         }
 
@@ -63,14 +63,14 @@ object InitialCompilerModule {
       })
 
     case Cons(head, tail) =>
-      Out.map2(compile(head), compile(tail)) { (compiledHead, compiledTail) =>
+      Out.map2(interpret(head), interpret(tail)) { (compiledHead, compiledTail) =>
         RNGRepr { rng =>
           (rng, Some((compiledHead, compiledTail)))
         }
       }
 
     case MapEmpty(ev1, ev2) =>
-      Out.map2(compile(ev1), compile(ev2)) { (compiled1, compiled2) =>
+      Out.map2(interpret(ev1), interpret(ev2)) { (compiled1, compiled2) =>
         RNGRepr { rng =>
           val (rng2, next) = compiled1.run(rng)
           next match {
@@ -81,7 +81,7 @@ object InitialCompilerModule {
       }
 
     case MapCons(eva, f) =>
-      Out.map2(compile(eva), compile(f)) { (compiledEva, compiledF) =>
+      Out.map2(interpret(eva), interpret(f)) { (compiledEva, compiledF) =>
         RNGRepr { rng =>
           val (rng2, next) = compiledEva.run(rng)
           next match {
@@ -92,7 +92,7 @@ object InitialCompilerModule {
       }
 
     case Uniform(from, to) =>
-      Out.map2(compile(from), compile(to)) { (compiledFrom, compiledTo) =>
+      Out.map2(interpret(from), interpret(to)) { (compiledFrom, compiledTo) =>
         lazy val self: RNGRepr[Double] = RNGRepr { rng =>
           val (n, rng2) = rng.nextInt
           val d = (n.toDouble - Int.MinValue) / (Int.MaxValue.toDouble - Int.MinValue.toDouble)
@@ -103,13 +103,13 @@ object InitialCompilerModule {
       }
 
     case UniformDiscrete(from, to, step) =>
-      Out.map3(compile(from), compile(to), compile(step)) { (f, t, s) =>
+      Out.map3(interpret(from), interpret(to), interpret(step)) { (f, t, s) =>
         uniformChoiceRepr((f to t by s).toList)
       }
 
     case UniformChoice(ts) =>
       ctx =>
-        uniformChoiceRepr(ts.map(t => compile(t)(ctx)))
+        uniformChoiceRepr(ts.map(t => interpret(t)(ctx)))
 
   }
 
@@ -134,14 +134,14 @@ object InitialCompilerModule {
 
   }
 
-  private def compile1[A, B](a: R[A])(f: A => B): Out[B] =
-    compile(a).map(f)
+  private def interpret1[A, B](a: R[A])(f: A => B): Out[B] =
+    interpret(a).map(f)
 
-  private def compile2[A, B, C](a: R[A], b: R[B])(f: (A, B) => C): Out[C] =
-    Out.map2(compile(a), compile(b))(f)
+  private def interpret2[A, B, C](a: R[A], b: R[B])(f: (A, B) => C): Out[C] =
+    Out.map2(interpret(a), interpret(b))(f)
 
-  private def compile3[A, B, C, D](a: R[A], b: R[B], c: R[C])(f: (A, B, C) => D): Out[D] =
-    Out.map3(compile(a), compile(b), compile(c))(f)
+  private def interpret3[A, B, C, D](a: R[A], b: R[B], c: R[C])(f: (A, B, C) => D): Out[D] =
+    Out.map3(interpret(a), interpret(b), interpret(c))(f)
 
   private object Out {
     def pure[T](t: T): Out[T] = ctx => t
