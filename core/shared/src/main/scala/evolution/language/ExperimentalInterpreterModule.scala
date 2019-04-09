d0efa4558eec1fd4483355b9aa2199f79ac92368
Nicolò Martini <nicolo@martini.io>
2019-04-09 18:10:01 +0100

    Evolution interpreter derivation in ExperimentalInterpreterModule

--- core/shared/src/main/scala/evolution/language/ExperimentalInterpreterModule.scala
+++ core/shared/src/main/scala/evolution/language/ExperimentalInterpreterModule.scala
@@ -1,8 +1,11 @@
 package evolution.language
-import InterpreterModule._
-import evolution.data.{ Ctx, ExpressionModule }
+import cats.Eval
 import evolution.data.EvaluationContext._
+import evolution.data.{ Ctx, ExpressionModule }
 import evolution.geometry.Point
+import evolution.language.InterpreterModule._
+import evolution.materialization.RNGRepr
+import shapeless.Lazy
 
 sealed trait Evo[T]
 
@@ -14,6 +17,8 @@ trait ExperimentalInterpreterModule extends ExpressionModule[Evo] {
   }
 
   object Interpreter {
+    def apply[T1, T2](implicit int: Interpreter[T1, T2]): Interpreter[T1, T2] = int
+
     def instance[T1, T2](f: Expr[T1] => Out[T2]): Interpreter[T1, T2] =
       (t1: Expr[T1]) => f(t1)
 
@@ -23,38 +28,72 @@ trait ExperimentalInterpreterModule extends ExpressionModule[Evo] {
     }
 
     implicit class Ops2[A1, A2](pairOfExprs: (Expr[A1], Expr[A2])) {
-      def interpret[T1, T2, T3](
+      def interpret2[T1, T2, T3](
         f: (T1, T2) => T3)(implicit int1: Interpreter[A1, T1], int2: Interpreter[A2, T2]): Out[T3] =
         Out.map2(pairOfExprs._1.interpret[T1], pairOfExprs._2.interpret[T2])(f)
     }
 
-    implicit val double: Interpreter[Double, Double] =
-      interpreter[Double, Double] {
+    implicit lazy val double: Interpreter[Double, Double] =
+      baseInterpreter[Double] {
         case Dbl(d)   => Out.pure(d)
-        case ToDbl(n) => n.interpret[Int].map(_.toDouble)
-        case X(p)     => p.interpret[Point].map(_.x)
-        case Y(p)     => p.interpret[Point].map(_.y)
-      }
+        case ToDbl(n) => n.interpret.map(_.toDouble)
+        case X(p)     => p.interpret.map(_.x)
+        case Y(p)     => p.interpret.map(_.y)
+      }(double)
 
-    implicit val integer: Interpreter[Int, Int] =
-      interpreter[Int, Int] {
+    implicit lazy val integer: Interpreter[Int, Int] =
+      baseInterpreter[Int] {
         case Integer(n) => Out.pure(n)
-        case Floor(d)   => d.interpret[Double].map(_.toInt)
+        case Floor(d)   => d.interpret.map(_.toInt)
+      }(integer)
+
+    implicit lazy val point: Interpreter[Point, Point] =
+      baseInterpreter[Point] {
+        case Pnt(x, y) => (x, y).interpret2(Point.apply)
+      }(point)
+
+    implicit def evolution[T1, T2](implicit inner: Interpreter[T1, T2]): Interpreter[Evo[T1], RNGRepr[T2]] =
+      Interpreter.instance[Evo[T1], RNGRepr[T2]] {
+        case Empty() => Out.pure(RNGRepr(rng => (rng, None)))
+        case Cons(head, tail) =>
+          val interpretedHead = head.interpret[T2]
+          val interpretedTail = tail.interpret[RNGRepr[T2]]
+          Contextual.instance { ctx =>
+            val h = interpretedHead(ctx)
+            RNGRepr { rng =>
+              (rng, Some((h, interpretedTail(ctx))))
+            }
+          }
+
+        case MapEmpty(ev1, ev2) =>
+          (ev1, ev2).interpret2[RNGRepr[T2], RNGRepr[T2], RNGRepr[T2]] { (compiled1, compiled2) =>
+            RNGRepr { rng =>
+              val (rng2, next) = compiled1.run(rng)
+              next match {
+                case None => compiled2.run(rng2)
+                case _    => (rng2, next)
+              }
+            }
+          }
+
+        case MapCons(eva, f) => ???
+//          (eva, f).interpret2[RNGRepr[T2], RNGRepr[T2], RNGRepr[T2]] { (compiledEva, compiledF) =>
+//            RNGRepr { rng =>
+//              val (rng2, next) = compiledEva.run(rng)
+//              next match {
+//                case None            => (rng2, None)
+//                case Some((a, eva2)) => compiledF(a)(eva2).run(rng2)
+//              }
+//            }
+//          }
       }
 
-    implicit val point: Interpreter[Point, Point] =
-      interpreter[Point, Point] {
-        case Pnt(x, y) => (x, y).interpret[Double, Double, Point](Point.apply)
-      }
-
-    private def common[T]: PartialFunction[Expr[T], Out[T]] = {
-      case Var(name) => Contextual.instance[T](get[Any](_, name).asInstanceOf[T])
-      //case add @ Add(a, b) => interpret2(a, b)(add.semigroup.combine)
+    private def common[T](int: => Interpreter[T, T]): PartialFunction[Expr[T], Out[T]] = {
+      case Var(name)       => Contextual.instance[T](get[Any](_, name).asInstanceOf[T])
+      case add @ Add(a, b) => (a, b).interpret2(add.semigroup.combine)(int, int)
     }
 
-    private def interpreter[T1, T2](f: PartialFunction[Expr[T1], Out[T2]]): Interpreter[T1, T2] =
-      Interpreter.instance[T1, T2](f orElse {
-        case Var(name) => Contextual.instance[T2](get[Any](_, name).asInstanceOf[T2])
-      })
+    private def baseInterpreter[T](f: PartialFunction[Expr[T], Out[T]])(self: => Interpreter[T, T]): Interpreter[T, T] =
+      Interpreter.instance[T, T](f orElse common[T](self))
   }
 }
