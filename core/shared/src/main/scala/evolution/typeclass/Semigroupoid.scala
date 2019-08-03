c4d5b5e31005dda40ef797ff91cf3aeededd9881
Nicolò Martini <nicolo@martini.io>
2019-08-03 10:13:29 +0100

    Semigrupoid typeclass

---
+++ core/shared/src/main/scala/evolution/typeclass/Semigroupoid.scala
@@ -0,0 +1,39 @@
+package evolution.typeclass
+
+import evolution.geometry.Point
+import evolution.materialization.Iterable
+
+trait Semigroupoid[A, B, C] { self =>
+  def combine(a: A, b: B): C
+  final def op: Semigroupoid[B, A, C] = new Semigroupoid[B, A, C] {
+    def combine(b: B, a: A): C = self.combine(a, b)
+  }
+}
+
+object Semigroupoid {
+  def apply[A, B, C](implicit sg: Semigroupoid[A, B, C]): Semigroupoid[A, B, C] = sg
+
+  object Multiplicative {
+    val dblDblDbl: Semigroupoid[Double, Double, Double] = (a, b) => a * b
+    val dblPointPoint: Semigroupoid[Double, Point, Point] = (a, b) => b * a
+    val pointDblPoint: Semigroupoid[Point, Double, Point] = (a, b) => a * b
+    val intIntInt: Semigroupoid[Int, Int, Int] = (a, b) => a * b
+    val intDblDbl: Semigroupoid[Int, Double, Double] = (a, b) => a * b
+    val dblIntDbl: Semigroupoid[Double, Int, Double] = (a, b) => a * b
+    val intPointPoint: Semigroupoid[Int, Point, Point] = (a, b) => b * a
+    val pointIntPoint: Semigroupoid[Point, Int, Point] = (a, b) => a * b
+    val dblEvoPointEvoPoint: Semigroupoid[Double, Iterable[Point], Iterable[Point]] = (a, b) => Iterable.map[Point, Point](b, _ * a)
+    val evoPointDblEvoPoint: Semigroupoid[Iterable[Point], Double, Iterable[Point]] = (a, b) => Iterable.map[Point, Point](a, _ * b)
+  }
+
+  object Additive {
+    val dblDblDbl: Semigroupoid[Double, Double, Double] = (a, b) => a + b
+    val intIntInt: Semigroupoid[Int, Int, Int] = (a, b) => a + b
+    val intDblDbl: Semigroupoid[Int, Double, Double] = (a, b) => a + b
+    val dblIntDbl: Semigroupoid[Double, Int, Double] = (a, b) => a + b
+    val evoDblEvoDblEvoDbl: Semigroupoid[Iterable[Double], Iterable[Double], Iterable[Double]] =
+      (a, b) => Iterable.zipWithUncurried[Double, Double, Double]((x, y) => x + y)(a, b)
+    val evoPointEvoPointEvoPoint: Semigroupoid[Iterable[Point], Iterable[Point], Iterable[Point]] =
+    (a, b) => Iterable.zipWithUncurried[Point, Point, Point]((x, y) => x + y)(a, b)
+  }
+}
