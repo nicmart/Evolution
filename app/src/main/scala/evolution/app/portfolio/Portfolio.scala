03a937cb876627c6dbfbc3db0f3621c62e9d4c79
Nicolò Martini <nicolo@martini.io>
2019-08-13 21:41:32 +0100

    Add some other drawings to portfolio

--- app/src/main/scala/evolution/app/portfolio/Portfolio.scala
+++ app/src/main/scala/evolution/app/portfolio/Portfolio.scala
@@ -5,6 +5,8 @@ import evolution.app.conf.Conf.defaultRendererState
 import evolution.app.model.DrawingRepository
 import cats.implicits._
 import cats.effect.IO
+import evolution.app.model.state.OffCanvasStrategy
+import evolution.app.model.state.InfiniteCanvas
 
 object Portfolio {
   val drawings = List(
@@ -14,10 +16,19 @@ object Portfolio {
       defaultRendererState
     ),
     Drawing(
+      Some("Simple Brownian"),
+      DrawingState(0L, """
+      |r = uniform(-2, 2) in
+      |integrate(point(0, 0), @point(r, r))
+      """.stripMargin),
+      defaultRendererState
+    ),
+    Drawing(
       Some("Normal Jumps"),
       DrawingState(
         0L,
-        """start = point(left, 0) in
+        """
+        |start = point(left, 0) in
         |jump = 150 in
         |r = 5 in
         |total = floor((right - left) / jump) in
@@ -37,14 +48,69 @@ object Portfolio {
       defaultRendererState
     ),
     Drawing(
-      Some("Simple Brownian"),
-      DrawingState(0L, """
-      |r = uniform(-2, 2) in
-      |integrate(point(0, 0), @point(r, r))
-      """.stripMargin),
+      Some("Differential Equation: x' = sin(x)"),
+      DrawingState(
+        0L,
+        """
+        |flatMap(
+        |  integrate(bottom, const(10)),
+        |  r -> take(
+        |    3000,
+        |    @point(
+        |      integrate(left, const(1)),
+        |      solve1(
+        |        const(x -> sin(0.01 * x)),
+        |        r
+        |      )
+        |    )
+        |  )
+        |)
+        |""".stripMargin
+      ),
+      defaultRendererWithInfiniteCanvas
+    ),
+    Drawing(
+      Some("Second Order Brownian"),
+      DrawingState(
+        0L,
+        """
+        |l = 0.2 in
+        |f = 0.01 in
+        |
+        |rndPoint = @point(uniform(-l, l), uniform(-l, l)) in
+        |solve2(
+        |  map(rndPoint, r -> x -> v -> r -f * v),
+        |  point(0, 0),
+        |  point(0, 0)
+        |)
+        |""".stripMargin
+      ),
       defaultRendererState
+    ),
+    Drawing(
+      Some("Comet"),
+      DrawingState(
+        0L,
+        """
+        |flatten(
+        |  const(
+        |    take(
+        |      200,
+        |      solve2(
+        |        map(@point(uniform(-0.8, 0.1), uniform(-0.8, 0.5)), r -> x -> v -> r + -0.01 * v),
+        |        point(0, 0),
+        |        point(0, 0)
+        |      )
+        |    )
+        |  )
+        |)
+        |""".stripMargin
+      ),
+      defaultRendererWithInfiniteCanvas
     )
   )
+
+  private lazy val defaultRendererWithInfiniteCanvas = defaultRendererState.copy(offCanvasSettings = InfiniteCanvas)
 
   def loadIntoRepository(repository: DrawingRepository): IO[Unit] =
     drawings.zipWithIndex.traverse { case (d, i) => repository.save(i.toString, d) }.void
