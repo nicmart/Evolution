3a6a409a80bdc36bafda2818fdb0975c2579faef
Nicolò Martini <nicolo@martini.io>
2019-08-21 09:20:48 +0100

    Some styling for the codemirror editor

--- app/src/main/scala/evolution/app/react/component/presentational/CodeMirror.scala
+++ app/src/main/scala/evolution/app/react/component/presentational/CodeMirror.scala
@@ -8,7 +8,7 @@ import scala.scalajs.js.annotation.JSImport
 
 object CodeMirror {
 
-  //@JSGlobal("Controlled")
+  //@JSGlobal("UnControlled")
   @JSImport("react-codemirror2", "UnControlled")
   @js.native
   object RawComponent extends js.Object
@@ -19,18 +19,34 @@ object CodeMirror {
   @js.native
   trait Data extends js.Object
 
+  trait Options extends js.Object {
+    var theme: String
+    var mode: String
+  }
+
+  object Options {
+    def apply(theme: String, mode: String): Options = {
+      val options = (new js.Object).asInstanceOf[Options]
+      options.theme = theme
+      options.mode = mode
+      options
+    }
+  }
+
   type OnChange = js.Function3[Editor, Data, String, Unit]
 
   @js.native
   trait Props extends js.Object {
     var value: String = js.native
     var onChange: OnChange = js.native
+    var options: Options = js.native
   }
 
   def props(value: String, onChange: (Editor, Data, String) => Callback): Props = {
     val p = (new js.Object).asInstanceOf[Props]
     p.value = value
     p.onChange = (editor: Editor, data: Data, value: String) => onChange(editor, data, value).runNow()
+    p.options = Options(theme = "evolution", mode = "string")
     p
   }
 
