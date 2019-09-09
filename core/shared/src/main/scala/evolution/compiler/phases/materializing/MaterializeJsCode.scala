7dd45e517fb09bcd58c20e2d50f1fa5ae866eac6
Nicolò Martini <nicolo@martini.io>
2019-09-09 13:25:35 +0100

    Add a minimalistic AST for Js expressions

--- core/shared/src/main/scala/evolution/compiler/phases/materializing/MaterializeJsCode.scala
+++ core/shared/src/main/scala/evolution/compiler/phases/materializing/MaterializeJsCode.scala
@@ -6,37 +6,43 @@ import Expr._
 // TODO this is an implementation
 object MaterializeJsCode {
 
-  def materialize[T](expr: Expr[T]): String = {
+  def materialize[T](expr: Expr[T]): JsExpr = {
     expr match {
-      case Dbl(d)     => d.toString
-      case Floor(d)   => s"Math.floor(${materialize(d)})"
-      case ToDbl(n)   => materialize(n)
-      case Integer(n) => n.toString
-      case Pnt(x, y)  => JsObj("x" -> materialize(x), "y" -> materialize(y)).json
+      case Dbl(d)     => JsExpr.Raw(d.toString)
+      case Floor(d)   => JsExpr.App(JsExpr.Raw("Math.floor"), List(materialize(d)))
+      case ToDbl(n)   => materialize(n) // There is only one numeric type in JS
+      case Integer(n) => JsExpr.Raw(n.toString)
+      case Pnt(x, y)  => JsExpr.Obj("x" -> materialize(x), "y" -> materialize(y))
 
-      case Var(name) => name
+      case Var(name) => JsExpr.Raw(name)
       case Let(variable, value, expr) =>
-        JsApp(JsLambda(List(variable), materialize(expr)).js, List(materialize(value))).js
+        JsExpr.App(JsExpr.Lambda(List(variable), materialize(expr)), List(materialize(value)))
       case Expr.Constant(t) => ???
     }
   }
 
-  private case class JsObj(fields: (String, String)*) {
-    def json: String =
-      fields.map { case (key, jsonValue) => s""""$key": $jsonValue""" }.mkString("{", ", ", "}")
+  sealed trait JsExpr {
+    def js: String
   }
 
-  private case class JsLambda(args: List[String], body: String) {
-    def js: String =
-      s"""
-      function(${args.mkString(", ")}) {
-        return $body;
-      }
-    """.stripMargin
-  }
+  object JsExpr {
+    case class Raw(js: String) extends JsExpr
+    case class Obj(fields: (String, JsExpr)*) extends JsExpr {
+      def js: String =
+        fields.map { case (key, jsExpr) => s""""$key": ${jsExpr.js}""" }.mkString("{", ", ", "}")
+    }
+    case class Lambda(args: List[String], body: JsExpr) extends JsExpr {
+      def js: String =
+        s"""
+        function(${args.mkString(", ")}) {
+          return ${body.js};
+        }
+      """.stripMargin
+    }
 
-  private case class JsApp(func: String, args: List[String]) {
-    def js: String =
-      s"($func)(${args.mkString(", ")})"
+    case class App(func: JsExpr, args: List[JsExpr]) extends JsExpr {
+      def js: String =
+        s"(${func.js})(${args.map(_.js).mkString(", ")})"
+    }
   }
 }
