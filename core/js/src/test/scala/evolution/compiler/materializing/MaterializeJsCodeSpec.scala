5689a721aff2c102eeedef0a26c8b80d59f81bc3
Nicolò Martini <nicolo@martini.io>
2019-09-09 21:22:24 +0100

    Make JsExpr private

--- core/js/src/test/scala/evolution/compiler/materializing/MaterializeJsCodeSpec.scala
+++ core/js/src/test/scala/evolution/compiler/materializing/MaterializeJsCodeSpec.scala
@@ -8,29 +8,29 @@ import scala.scalajs.js
 import evolution.compiler.phases.materializing.MaterializeJsCode.JsExpr
 
 class MaterializeJsCodeSpec extends LanguageSpec {
-  "Materializing stuff" - {
-    "minimal materialization" in {
-      val jsExpr = MaterializeJsCode.materialize(Expr.Dbl(1.23))
-      val result = evaluate(jsExpr)
+  "Materializing expressions" - {
+    "materialize doubles" in {
+      val jsCode = MaterializeJsCode.materialize(Expr.Dbl(1.23))
+      val result = evaluate(jsCode)
       result shouldBe 1.23
     }
 
     "materialize points" in {
-      val jsExpr = MaterializeJsCode.materialize(Expr.Pnt(Expr.Dbl(1), Expr.Dbl(2)))
-      val result = evaluate(jsExpr).asInstanceOf[js.Dictionary[Double]]
+      val jsCode = MaterializeJsCode.materialize(Expr.Pnt(Expr.Dbl(1), Expr.Dbl(2)))
+      val result = evaluate(jsCode).asInstanceOf[js.Dictionary[Double]]
       result("x") shouldBe 1.0
       result("y") shouldBe 2.0
     }
 
     "materialize constant evolutions" in {
-      val jsExpr = MaterializeJsCode.materialize(Expr.Constant(Expr.Dbl(1.1)))
-      val result = evaluate(jsExpr).asInstanceOf[js.Iterable[Double]]
+      val jsCode = MaterializeJsCode.materialize(Expr.Constant(Expr.Dbl(1.1)))
+      val result = evaluate(jsCode).asInstanceOf[js.Iterable[Double]]
       result.iterator.take(10).toList shouldBe List.fill(10)(1.1)
     }
   }
 
-  private def evaluate(expr: JsExpr): Any = {
-    val f = new Function(s"return ${expr.js};")
+  private def evaluate(expr: String): Any = {
+    val f = new Function(s"return $expr;")
     f.call((), ())
   }
 }
--- core/shared/src/main/scala/evolution/compiler/phases/materializing/MaterializeJsCode.scala
+++ core/shared/src/main/scala/evolution/compiler/phases/materializing/MaterializeJsCode.scala
@@ -6,26 +6,29 @@ import Expr._
 // TODO this is an implementation
 object MaterializeJsCode {
 
-  def materialize[T](expr: Expr[T]): JsExpr = {
+  def materialize[T](expr: Expr[T]): String =
+    toJs(expr).js
+
+  private def toJs[T](expr: Expr[T]): JsExpr = {
     expr match {
       case Dbl(d)     => JsExpr.Raw(d.toString)
-      case Floor(d)   => JsExpr.App(JsExpr.Raw("Math.floor"), List(materialize(d)))
-      case ToDbl(n)   => materialize(n) // There is only one numeric type in JS
+      case Floor(d)   => JsExpr.App(JsExpr.Raw("Math.floor"), List(toJs(d)))
+      case ToDbl(n)   => toJs(n) // There is only one numeric type in JS
       case Integer(n) => JsExpr.Raw(n.toString)
-      case Pnt(x, y)  => JsExpr.Obj("x" -> materialize(x), "y" -> materialize(y))
+      case Pnt(x, y)  => JsExpr.Obj("x" -> toJs(x), "y" -> toJs(y))
 
       case Var(name) => JsExpr.Raw(name)
       case Let(variable, value, expr) =>
-        JsExpr.App(JsExpr.Lambda(List(variable), materialize(expr)), List(materialize(value)))
-      case Expr.Constant(t) => JsExpr.Iterable(JsExpr.Raw(s"while(true) { yield ${materialize(t).js}; }"))
+        JsExpr.App(JsExpr.Lambda(List(variable), toJs(expr)), List(toJs(value)))
+      case Expr.Constant(t) => JsExpr.Iterable(JsExpr.Raw(s"while(true) { yield ${toJs(t).js}; }"))
     }
   }
 
-  sealed trait JsExpr {
+  private sealed trait JsExpr {
     def js: String
   }
 
-  object JsExpr {
+  private object JsExpr {
     case class Raw(code: String) extends JsExpr {
       def js: String = code.trim
     }
