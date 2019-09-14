22940fd849288abf3021f9975574b19025b2cd1e
Nicolò Martini <nicolo@martini.io>
2019-09-14 09:25:29 +0100

    Move Type methods into ops class

--- core/shared/src/main/scala/evolution/compiler/types/Type.scala
+++ core/shared/src/main/scala/evolution/compiler/types/Type.scala
@@ -6,25 +6,6 @@ import evolution.materialization.Evolution
 
 sealed trait Type {
   type Out
-
-  final def children: List[Type] = this match {
-    case Type.Evo(inner)      => List(inner)
-    case Type.Lst(inner)      => List(inner)
-    case Type.Arrow(from, to) => List(from, to)
-    case _                    => Nil
-  }
-
-  final def =>:(from: Type): Type = Type.Arrow(from, this)
-
-  final def typeVars: Set[Type.Var] = this match {
-    case Type.Var(name) => Set(Type.Var(name))
-    case _              => children.flatMap(_.typeVars).toSet
-  }
-
-  final def typeVarUsages(varName: String): List[Type] =
-    typeVars.collect {
-      case tpe @ Type.Var(name) if varName == name => tpe
-    }.toList
 }
 
 object Type {
@@ -32,7 +13,6 @@ object Type {
     type Out = Nothing
     override def toString: String = name
   }
-
   final case object Integer extends Type { type Out = Int }
   final case object Double extends Type { type Out = Double }
   final case object Point extends Type { type Out = Point }
@@ -45,12 +25,32 @@ object Type {
   }
 
   implicit class TypeOps(val self: Type) extends AnyVal {
-    def unwrapEvo: Either[String, Type] = self match {
+
+    final def children: List[Type] = self match {
+      case Type.Evo(inner)      => List(inner)
+      case Type.Lst(inner)      => List(inner)
+      case Type.Arrow(from, to) => List(from, to)
+      case _                    => Nil
+    }
+
+    final def =>:(from: Type): Type = Type.Arrow(from, self)
+
+    final def typeVars: Set[Type.Var] = self match {
+      case Type.Var(name) => Set(Type.Var(name))
+      case _              => children.flatMap(_.typeVars).toSet
+    }
+
+    final def typeVarUsages(varName: String): List[Type] =
+      typeVars.collect {
+        case tpe @ Type.Var(name) if varName == name => tpe
+      }.toList
+
+    final def unwrapEvo: Either[String, Type] = self match {
       case Type.Evo(inner) => inner.asRight
       case _               => s"Type $self is not an Evolution type".asLeft
     }
 
-    def unwrapLst: Either[String, Type] = self match {
+    final def unwrapLst: Either[String, Type] = self match {
       case Type.Lst(inner) => inner.asRight
       case _               => s"Type $self is not a Lst type".asLeft
     }
