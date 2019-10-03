29087975bcf56844e1f4ef8f9b383a024ad1a36a
Nicolò Martini <nicolo@martini.io>
2019-10-03 08:43:42 +0100

    Add a damped oscillator to stdlib

--- core/src/main/scala/evolution/compiler/stdlib/StdLib.scala
+++ core/src/main/scala/evolution/compiler/stdlib/StdLib.scala
@@ -4,46 +4,54 @@ object StdLib {
   val code = """
 
 grid(gridSize) = product(
-    y <- range(top, bottom, -gridSize),
-    x <- range(left, right, gridSize)
+  y <- range(top, bottom, -gridSize),
+  x <- range(left, right, gridSize)
 ) in point(x, y)
 in
 
 randomPointInGrid(gridSize) = @point(
-    uniformDiscrete(left, right, gridSize),
-    uniformDiscrete(bottom, top, gridSize)
+  uniformDiscrete(left, right, gridSize),
+  uniformDiscrete(bottom, top, gridSize)
 ) in
 
 randomPoint = @point(
-    uniform(left, right),
-    uniform(bottom, top)
+  uniform(left, right),
+  uniform(bottom, top)
 ) in
 
 uniformPoint(r) = @point(
-    uniform(-r, r),
-    uniform(-r, r)
+  uniform(-r, r),
+  uniform(-r, r)
 ) in
 
 line(from, to, step) =
-    total = floor(norm(to - from) / step) + 1 in
-    v = versor(to - from) in
-    map(
-        range(0, total, 1),
-        s -> from + s * step * v
-    )
+  total = floor(norm(to - from) / step) + 1 in
+  v = versor(to - from) in
+  map(
+    range(0, total, 1),
+    s -> from + s * step * v
+  )
 in
 
 arc(r, a1, a2, w) = map(
-    range(a1, a2, w),
-    a -> polar(r, a)
+  range(a1, a2, w),
+  a -> polar(r, a)
 ) in
 
 finiteCircle(r, w) = arc(r, 0, 2 * pi, w) in
 
 circle(r, w) = map(
-    integrate(0, const(w)),
-    a -> polar(r, a)
+  integrate(0, const(w)),
+  a -> polar(r, a)
 ) in
+
+dampedOscillator(a, b, rnd) = 
+  solve2(
+    map(rnd, r -> x -> v -> r + a * x + b * v),
+    point(0, 0),
+    point(0, 0)
+  )
+in
 
 export
 
