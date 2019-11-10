af16a08ed13a12aac81ddc64b9bd25ddf869b07c
Nicolò Martini <nicolo@martini.io>
2019-11-10 17:50:47 +0000

    Implement a typer implementation that solve predicates constraints

---
+++ core/src/main/scala/evolution/compiler/phases/typer/PredicatesSolverTyper.scala
@@ -0,0 +1,18 @@
+package evolution.compiler.phases.typer
+
+import evolution.compiler.phases.Typer
+import evolution.compiler.phases.typer.config.TypingConfig
+import evolution.compiler.tree.{Tree, TypedTree}
+import evolution.compiler.types.{Assumptions, Type}
+
+final class PredicatesSolverTyper(typer: Typer, solver: UnifyPredicates) extends Typer {
+  override def typeTree(
+      tree: Tree,
+      expectedType: Option[Type],
+      assumptions: Assumptions
+  ): Either[String, TypedTree] =
+    for {
+      typedTree <- typer.typeTree(tree, expectedType, assumptions)
+      predicatesSubstitution <- solver.unify(TypingConfig.instancesPredicates, typedTree.annotation.predicates)
+    } yield predicatesSubstitution.substitute(typedTree)
+}
