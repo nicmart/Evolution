cbe0554a65dc8cf4bdbf5b351a8e9b060637a88f
Nicolò Martini <nicolo@martini.io>
2019-11-17 10:22:05 +0000

    Move unification algorithm outside RecursiveTyper

--- core/src/main/scala/evolution/compiler/phases/typer/RecursiveTyper.scala
+++ core/src/main/scala/evolution/compiler/phases/typer/RecursiveTyper.scala
@@ -101,26 +101,6 @@ final class RecursiveTyper extends Typer {
 }
 
 object RecursiveTyper {
-  // TODO move outside
-  private def mostGeneralUnifier(a: Type, b: Type): Either[String, Substitution] = (a, b) match {
-    case (Type.Arrow(a1, a2), Type.Arrow(b1, b2)) =>
-      for {
-        s1 <- mostGeneralUnifier(a1, b1)
-        s2 <- mostGeneralUnifier(s1.substitute(a2), s1.substitute(b2))
-      } yield s1.andThen(s2)
-    case (Type.Var(x), t)           => varBind(x, t)
-    case (t, Type.Var(x))           => varBind(x, t)
-    case (Type.Lst(a), Type.Lst(b)) => mostGeneralUnifier(a, b)
-    case (Type.Evo(a), Type.Evo(b)) => mostGeneralUnifier(a, b)
-    case _ if a == b                => Right(Substitution.empty)
-    case _                          => Left(s"$a can't be unified with $b")
-  }
-
-  private def varBind(name: String, tpe: Type): Either[String, Substitution] =
-    if (tpe == Type.Var(name)) Right(Substitution.empty)
-    else if (tpe.typeVarUsages(name).isEmpty) Right(Substitution(name -> tpe))
-    else Left(s"$name cannot be bound to $tpe")
-
   private def qualified(predicates: List[Predicate], tpe: Type): Qualified[Type] =
     Qualified(predicates.distinct, tpe)
 
@@ -175,7 +155,7 @@ object RecursiveTyper {
       for {
         currentSubstitution <- substitution
         unifyingSubstitution <- fromEither(
-          mostGeneralUnifier(currentSubstitution.substitute(t1), currentSubstitution.substitute(t2))
+          Unifier.mostGeneralUnifier(currentSubstitution.substitute(t1), currentSubstitution.substitute(t2))
         )
         _ <- setSubstitution(currentSubstitution.andThen(unifyingSubstitution))
       } yield ()
