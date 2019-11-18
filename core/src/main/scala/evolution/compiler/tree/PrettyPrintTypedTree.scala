6fcc256aa08d63dd7e7fe040c5adcdb9433f9097
Nicolò Martini <nicolo@martini.io>
2019-11-18 09:20:47 +0000

    Print predicates in TypedTree Pretty Printer

--- core/src/main/scala/evolution/compiler/tree/PrettyPrintTypedTree.scala
+++ core/src/main/scala/evolution/compiler/tree/PrettyPrintTypedTree.scala
@@ -7,6 +7,6 @@ object PrettyPrintTypedTree {
   def apply(tree: TypedTree): String = AnnotatedTree.catamorphism(prettyPrintTreeF)(tree)
 
   private def prettyPrintTreeF(tpe: Qualified[Type], treeF: TreeF[String]): String =
-    s"${PrettyPrintTree.prettyPrintTreeF(treeF)}: ${tpe.value}"
+    s"${PrettyPrintTree.prettyPrintTreeF(treeF)}: $tpe"
 
 }
