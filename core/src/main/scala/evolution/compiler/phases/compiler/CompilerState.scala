3fafd92faddc093dbf7c0a9342f10e3aeeed7e25
Nicolò Martini <nicolo@martini.io>
2019-11-19 20:44:23 +0000

    Sketch a Compilation Monad

---
+++ core/src/main/scala/evolution/compiler/phases/compiler/CompilerState.scala
@@ -0,0 +1,14 @@
+package evolution.compiler.phases.compiler
+
+import evolution.compiler.tree.TypedTree
+
+private[compiler] case class CompilerState(bindings: Map[String, TypedTree]) {
+  def withBinding(name: String, expr: TypedTree): CompilerState =
+    CompilerState(bindings.updated(name, expr))
+
+  def binding(name: String): Option[TypedTree] = bindings.get(name)
+}
+
+private[compiler] object CompilerState {
+  val empty: CompilerState = CompilerState(Map.empty)
+}
