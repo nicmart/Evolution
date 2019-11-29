63e92366b0f9fa9b174d6ef7f17a1552cf4e909b
Nicolò Martini <nicolo@martini.io>
2019-11-29 15:59:19 +0000

    Improve PrettyPrinting

--- core/src/main/scala/evolution/compiler/tree/PrettyPrintTree.scala
+++ core/src/main/scala/evolution/compiler/tree/PrettyPrintTree.scala
@@ -3,6 +3,7 @@ package evolution.compiler.tree
 import TreeF._
 
 object PrettyPrintTree {
+  private val maxChildrenLegnth = 100
   def apply(tree: Tree): String = Tree.catamorphism(prettyPrintTreeF)(tree)(0)
 
   private[tree] def prettyPrintTreeF(treeF: TreeF[Int => String]): Int => String = n => {
@@ -10,7 +11,7 @@ object PrettyPrintTree {
     indent + (treeF match {
       case TreeF.App(g, args) => ppFunc("App", g :: args.toList, n)
       case Let(id, expr, in)  => ppFunc("Let", List(n => s"${"  " * n}$id", expr, in), n)
-      case Lambda(id, expr)   => s"$id -> " + ppFunc("", List(expr), n, "{", "}")
+      case Lambda(id, expr)   => ppLambda(id, expr, n)
       case id: Id             => id.name
       case DoubleLiteral(d)   => d.toString
       case Bool(b)            => b.toString
@@ -19,15 +20,19 @@ object PrettyPrintTree {
     })
   }
 
+  private def ppLambda(id: String, expr: Int => String, n: Int): String =
+    ppFunc(s"$id -> ", List(expr), n, "{", "}")
+
   private def ppFunc(
       name: String,
       children: List[Int => String],
       n: Int,
       open: String = "(",
-      close: String = ")",
-      newLine: String = "\n"
+      close: String = ")"
   ): String = {
     val indent = "  " * n
-    children.map(child => child(n + 1)).mkString(s"$name$open$newLine", s",$newLine", s"$newLine$indent$close")
+    val childrenLength = children.map(_.apply(0)).mkString(", ").length
+    if (childrenLength < maxChildrenLegnth) children.map(_.apply(0)).mkString(s"$name$open", ", ", close)
+    else children.map(_.apply(n + 1)).mkString(s"$name$open\n", ",\n", s"\n$indent$close")
   }
 }
