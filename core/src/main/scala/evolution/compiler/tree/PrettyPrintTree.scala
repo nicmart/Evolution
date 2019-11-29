6d0382dd235f919bb57466ec9561defc420ff1a5
Nicolò Martini <nicolo@martini.io>
2019-11-29 15:18:03 +0000

    Prettier PrettyPrinter

--- core/src/main/scala/evolution/compiler/tree/PrettyPrintTree.scala
+++ core/src/main/scala/evolution/compiler/tree/PrettyPrintTree.scala
@@ -3,19 +3,31 @@ package evolution.compiler.tree
 import TreeF._
 
 object PrettyPrintTree {
-  def apply(tree: Tree): String = Tree.catamorphism(prettyPrintTreeF)(tree)
+  def apply(tree: Tree): String = Tree.catamorphism(prettyPrintTreeF)(tree)(0)
 
-  private[tree] def prettyPrintTreeF(treeF: TreeF[String]): String = treeF match {
-    case TreeF.App(g, args) => ppFunc(s"($g)", args.toList)
-    case Let(id, expr, in)  => ppFunc("Let", List(id, expr, in))
-    case Lambda(id, expr)   => s"$id -> $expr"
-    case id: Id             => id.name
-    case DoubleLiteral(d)   => d.toString
-    case Bool(b)            => b.toString
-    case IntLiteral(n)      => n.toString
-    case Lst(ts)            => ppFunc("Lst", ts)
+  private[tree] def prettyPrintTreeF(treeF: TreeF[Int => String]): Int => String = n => {
+    val indent = "  " * n
+    indent + (treeF match {
+      case TreeF.App(g, args) => ppFunc("App", g :: args.toList, n)
+      case Let(id, expr, in)  => ppFunc("Let", List(n => s"${"  " * n}$id", expr, in), n)
+      case Lambda(id, expr)   => s"$id -> " + ppFunc("", List(expr), n, "{", "}")
+      case id: Id             => id.name
+      case DoubleLiteral(d)   => d.toString
+      case Bool(b)            => b.toString
+      case IntLiteral(n)      => n.toString
+      case Lst(ts)            => ppFunc("Lst", ts, n)
+    })
   }
 
-  private def ppFunc(name: String, children: List[String]): String =
-    children.mkString(s"$name(", ", ", ")")
+  private def ppFunc(
+      name: String,
+      children: List[Int => String],
+      n: Int,
+      open: String = "(",
+      close: String = ")",
+      newLine: String = "\n"
+  ): String = {
+    val indent = "  " * n
+    children.map(child => child(n + 1)).mkString(s"$name$open$newLine", s",$newLine", s"$newLine$indent$close")
+  }
 }
--- core/src/main/scala/evolution/compiler/tree/PrettyPrintTypedTree.scala
+++ core/src/main/scala/evolution/compiler/tree/PrettyPrintTypedTree.scala
@@ -4,9 +4,9 @@ import evolution.compiler.types.Type
 import evolution.compiler.types.TypeClasses.Qualified
 
 object PrettyPrintTypedTree {
-  def apply(tree: TypedTree): String = AnnotatedTree.catamorphism(prettyPrintTreeF)(tree)
+  def apply(tree: TypedTree): String = AnnotatedTree.catamorphism(prettyPrintTreeF)(tree)(0)
 
-  private def prettyPrintTreeF(tpe: Qualified[Type], treeF: TreeF[String]): String =
-    s"${PrettyPrintTree.prettyPrintTreeF(treeF)}: $tpe"
+  private def prettyPrintTreeF(tpe: Qualified[Type], treeF: TreeF[Int => String]): Int => String =
+    n => s"${PrettyPrintTree.prettyPrintTreeF(treeF)(n)}: $tpe"
 
 }
