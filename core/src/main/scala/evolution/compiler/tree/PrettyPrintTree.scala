40123d845fe1d995d74811c42577aa71149f79a6
Nicolò Martini <nicolo@martini.io>
2019-11-29 22:03:09 +0000

    Improve PrettyPrinting

--- core/src/main/scala/evolution/compiler/tree/PrettyPrintTree.scala
+++ core/src/main/scala/evolution/compiler/tree/PrettyPrintTree.scala
@@ -6,33 +6,29 @@ object PrettyPrintTree {
   private val maxChildrenLegnth = 100
   def apply(tree: Tree): String = Tree.catamorphism(prettyPrintTreeF)(tree)(0)
 
-  private[tree] def prettyPrintTreeF(treeF: TreeF[Int => String]): Int => String = n => {
-    val indent = "  " * n
-    indent + (treeF match {
-      case TreeF.App(g, args) => ppFunc("App", g :: args.toList, n)
-      case Let(id, expr, in)  => ppFunc("Let", List(n => s"${"  " * n}$id", expr, in), n)
-      case Lambda(id, expr)   => ppLambda(id, expr, n)
-      case id: Id             => id.name
-      case DoubleLiteral(d)   => d.toString
-      case Bool(b)            => b.toString
-      case IntLiteral(n)      => n.toString
-      case Lst(ts)            => ppFunc("Lst", ts, n)
-    })
+  private[tree] def prettyPrintTreeF(treeF: TreeF[Int => String]): Int => String =
+    n =>
+      indent(n) + (treeF match {
+        case TreeF.App(g, args) => ppFunc("App", g :: args.toList, n)
+        case Let(id, expr, in)  => ppFunc("Let", List(n => s"${"  " * n}$id", expr, in), n)
+        case Lambda(id, expr)   => ppLambda(id, expr, n)
+        case id: Id             => id.name
+        case DoubleLiteral(d)   => d.toString
+        case Bool(b)            => b.toString
+        case IntLiteral(n)      => n.toString
+        case Lst(ts)            => ppFunc("Lst", ts, n)
+      })
+
+  private def ppLambda(id: String, expr: Int => String, n: Int): String = {
+    val childrenLength = expr(0).length
+    if (childrenLength < maxChildrenLegnth) s"$id -> { ${expr(0)} }"
+    else s"$id -> {\n${expr(n + 1)}\n${indent(n)}}"
   }
 
-  private def ppLambda(id: String, expr: Int => String, n: Int): String =
-    ppFunc(s"$id -> ", List(expr), n, "{", "}")
-
-  private def ppFunc(
-      name: String,
-      children: List[Int => String],
-      n: Int,
-      open: String = "(",
-      close: String = ")"
-  ): String = {
-    val indent = "  " * n
+  private def ppFunc(name: String, children: List[Int => String], n: Int): String = {
     val childrenLength = children.map(_.apply(0)).mkString(", ").length
-    if (childrenLength < maxChildrenLegnth) children.map(_.apply(0)).mkString(s"$name$open", ", ", close)
-    else children.map(_.apply(n + 1)).mkString(s"$name$open\n", ",\n", s"\n$indent$close")
+    if (childrenLength < maxChildrenLegnth) children.map(_.apply(0)).mkString(s"$name(", ", ", ")")
+    else children.map(_.apply(n + 1)).mkString(s"$name(\n", ",\n", s"\n${indent(n)})")
   }
+  private def indent(n: Int): String = "  " * n
 }
