ffcbb82732fbd1814fd3c4969d9e6d56403f9cdb
Nicolò Martini <nicolo@martini.io>
2019-12-06 19:43:00 +0000

    Refactor literal compilation

--- core/src/main/scala/evolution/compiler/term/TreeToTermCompiler.scala
+++ core/src/main/scala/evolution/compiler/term/TreeToTermCompiler.scala
@@ -18,31 +18,27 @@ class TreeToTermCompiler {
     val AnnotatedTree(Qualified(predicates, tpe), tree) = typedTree
 
     tree match {
-      case TreeF.Id(name, primitive) =>
-        traverse(predicates)(argFromPred).map(pArgs => pApp(Id(name), pArgs))
+      case TreeF.Id(name, primitive) => appPredicates(Id(name), predicates)
+      case TreeF.IntLiteral(n)       => appPredicates(Lit(LitInt(n)), predicates)
+      case TreeF.DoubleLiteral(n)    => appPredicates(Lit(LitDouble(n)), predicates)
+      case TreeF.Bool(b)             => appPredicates(Lit(LitBool(b)), predicates)
+
       case TreeF.Lambda(varName, expr)  => ???
       case TreeF.App(f, args)           => ???
       case TreeF.Let(varName, expr, in) => ???
-
-      case TreeF.IntLiteral(n) =>
-        predicates match {
-          case List(predicate @ Predicate("Num", List(pTpe))) if tpe == pTpe =>
-            argFromPred(predicate).map(pArg => PApp(Lit(LitInt(n)), pArg))
-          case _ => error(s"Unexpected Type Error for Int Literal")
-        }
-
-      case TreeF.DoubleLiteral(n) => pure(Lit(LitDouble(n)))
-      case TreeF.Bool(b)          => pure(Lit(LitBool(b)))
 
       case TreeF.Lst(ts) => traverse(ts)(compileM).map(ts => Lit(LitList(ts)))
     }
   }
 
+  private def appPredicates(term: Term, predicates: List[Predicate]): Compilation[Term] =
+    traverse(predicates)(argFromPred).map(pArgs => appPArgs(term, pArgs))
+
   private def argFromPred(predicate: Predicate): Compilation[PArg] =
     if (predicate.hasTypeVars) predName(predicate).map(PVar.apply)
     else fromEither(TypingConfig.instance(predicate).map(PInst.apply))
 
-  private def pApp(term: Term, pArgs: List[PArg]): Term =
+  private def appPArgs(term: Term, pArgs: List[PArg]): Term =
     pArgs.foldLeft(term)(PApp)
 }
 
