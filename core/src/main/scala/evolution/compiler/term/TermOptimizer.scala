6d94ea7d26f408933f98a6d1b998076a9563bc8c
Nicolò Martini <nicolo@martini.io>
2019-12-12 21:04:20 +0000

    Optimize applications of constants

--- core/src/main/scala/evolution/compiler/term/TermOptimizer.scala
+++ core/src/main/scala/evolution/compiler/term/TermOptimizer.scala
@@ -1,13 +1,24 @@
 package evolution.compiler.term
 
-import Term._
-import evolution.compiler.term.Term.Literal.LitInt
+import evolution.compiler.term.Term._
+
+import scala.util.Try
 
 final class TermOptimizer(interpreter: TermInterpreter) {
-  def optimize(term: Term): Term = term match {
+  def optimize(term: Term): Term = {
+    val optimized = term match {
+      case Lit(_) | Inst(_) => Value(interpreter.interpret(term))
+      case App(f, x) =>
+        (optimize(f), optimize(x)) match {
+          case (Value(f), Value(x)) => Value(f.asInstanceOf[Any => Any](x))
+          case (f, x)               => App(f, x)
+        }
 
-    case App(Lit(LitInt(_)), Inst(_)) => Value(interpreter.interpret(term))
+      case Id(_)              => Try(interpreter.interpret(term)).toOption.fold(term)(Value.apply)
+      case Lambda(name, body) => Lambda(name, optimize(body))
+      case _                  => term
+    }
 
-    case _ => term
+    optimized
   }
 }
--- core/src/test/scala/evolution/compiler/term/TermOptimizerTest.scala
+++ core/src/test/scala/evolution/compiler/term/TermOptimizerTest.scala
@@ -5,16 +5,33 @@ import evolution.compiler.types.TypeClassInstance.NumericInst
 import Term._
 import Literal._
 import evolution.compiler.phases.typer.config.TypingConfig
-import evolution.compiler.types.Type
+import evolution.compiler.types.{Type, TypeClassInstance}
 import evolution.compiler.types.TypeClasses.Predicate
 
 class TermOptimizerTest extends LanguageSpec {
-  "TermOptimizer should optimize" - {
-    "application of literals" - {
+  "TermOptimizer should" - {
+    "optimize application of literals" - {
       "integers" in {
-        val term = App(Lit(LitInt(2)), Inst(numDouble))
+        optimize(litInt(2)) shouldBe Value(2)
+      }
+    }
 
-        optimize(term) shouldBe Value(2)
+    "optimize sums of constants" - {
+      "2 + 3" in {
+        val term = App(App(App(Id("add"), Inst(addDouble)), litInt(2)), litInt(3))
+        optimize(term) shouldBe Value(5)
+      }
+
+      "2 + 3 + 4 + 5" in {
+        val term = add(addDouble, add(addDouble, add(addDouble, litInt(2), litInt(3)), litInt(4)), litInt(5))
+        optimize(term) shouldBe Value(14)
+      }
+    }
+
+    "optimize children" - {
+      "of lambdas" in {
+        val term = Lambda("x", App(Lit(LitInt(2)), Inst(numDouble)))
+        optimize(term) shouldBe Lambda("x", Value(2))
       }
     }
   }
@@ -22,6 +39,10 @@ class TermOptimizerTest extends LanguageSpec {
   lazy val optimizer = new TermOptimizer(new TermInterpreter)
 
   def optimize(term: Term): Term = optimizer.optimize(term)
+  def litInt(n: Int): Term = App(Lit(LitInt(n)), Inst(numDouble))
+  def add(inst: TypeClassInstance, a: Term, b: Term): Term =
+    App(App(App(Id("add"), Inst(inst)), a), b)
 
-  lazy val numDouble = TypingConfig.instance(Predicate("Num", List(Type.Double))).unsafeRight
+  lazy val numDouble: TypeClassInstance = TypingConfig.instance(Predicate("Num", List(Type.Double))).unsafeRight
+  lazy val addDouble = TypingConfig.instance(Predicate("Add", List(Type.Double, Type.Double, Type.Double))).unsafeRight
 }
