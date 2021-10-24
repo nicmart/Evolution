75c9e09e4c9b1e95e391f41d3f963620acaaf62b
Nicolò Martini <nicolo@martini.io>
2021-10-24 19:59:39 +0200

    Term as an enum

--- core/src/main/scala/evolution/compiler/term/Term.scala
+++ core/src/main/scala/evolution/compiler/term/Term.scala
@@ -1,21 +1,20 @@
 package evolution.compiler.term
 
+import evolution.compiler.term.Term.Literal
 import evolution.compiler.types.TypeClassInstance
 
-sealed trait Term
+enum Term:
+  case Lit(l: Literal)
+  case Id(name: String)
+  case Inst(instance: TypeClassInstance)
+  case Let(name: String, expr: Term, body: Term)
+  case Lambda(name: String, body: Term)
+  case Apply(f: Term, x: Term)
+  case Value(value: Any)
 
 object Term:
-  case class Lit(l: Literal) extends Term
-  case class Id(name: String) extends Term
-  case class Inst(instance: TypeClassInstance) extends Term
-  case class Let(name: String, expr: Term, body: Term) extends Term
-  case class Lambda(name: String, body: Term) extends Term
-  case class Apply(f: Term, x: Term) extends Term
-  case class Value(value: Any) extends Term
-
-  sealed abstract class Literal
-  object Literal:
-    case class LitInt(n: Int) extends Literal
-    case class LitBool(b: Boolean) extends Literal
-    case class LitDouble(d: Double) extends Literal
-    case class LitList(ts: List[Term]) extends Literal
+  enum Literal:
+    case LitInt(n: Int)
+    case LitBool(b: Boolean)
+    case LitDouble(d: Double)
+    case LitList(ts: List[Term])
